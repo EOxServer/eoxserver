2e699254a54d73f592b87e2d36c4753138e862c9
Stephan Krause <stephan.krause@eox.at>
2011-05-11 13:41:59 +0000

    removed "EOxS" in class names of Data Integration Layer corrected imports
    added exceptions
    
    fixed bug in core.interfaces (__iconf__ is now added to the Interface class
      if INTF_CONF is lacking)
    
    fixed bugs in core.registry and core.system
     * revised RegistryConfigReader so that it does not fail if optional
       configuration values are missing
     * replaced some occurrences of 'cls' in the load() method of Registry with
       'self'
     * ...

---
+++ autotest/urls.py
@@ -0,0 +1,62 @@
+#-----------------------------------------------------------------------
+# $Id$
+#
+# This software is named EOxServer, a server for Earth Observation data.
+#
+# Copyright (C) 2011 EOX IT Services GmbH
+# Authors: Stephan Krause, Stephan Meissl
+#
+# This file is part of EOxServer <http://www.eoxserver.org>.
+#
+#    EOxServer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License,
+#    or (at your option) any later version.
+#
+#    EOxServer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with EOxServer. If not, see <http://www.gnu.org/licenses/>.
+#
+#-----------------------------------------------------------------------
+
+from django.conf.urls.defaults import *
+
+# Enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+from django.contrib import databrowse
+from django.views.static import serve
+from django.conf import settings
+
+from eoxserver.services import views
+
+#from eoxserver.server.models import *
+
+#databrowse.site.register(EOxSCoverageEOCollectionRecord)
+#databrowse.site.register(EOxSCoverageSingleFileRecord)
+#databrowse.site.register(EOxSCoverageSingleFileNonGeoRecord)
+#databrowse.site.register(EOxSRangeType)
+#databrowse.site.register(EOxSRectifiedGridRecord)
+#databrowse.site.register(EOxSChannelRecord)
+#databrowse.site.register(EOxSDataDirRecord)
+#databrowse.site.register(EOxSLayerMetadataRecord)
+#databrowse.site.register(EOxSRangeType2Channel)
+
+urlpatterns = patterns('',
+    (r'^ows', views.ows),
+    # Example:
+    # (r'^eoxserver/', include('eoxserver.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+#    (r'^databrowse/(.*)', databrowse.site.root)
+)
