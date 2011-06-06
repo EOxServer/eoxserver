b81cd79091289a4bfd2a1f6a412dd498fcfedbe0
Stephan Meißl <stephan.meissl@eox.at>
2011-06-06 10:15:27 +0000

    Adjusting EOxServer URL to http://eoxserver.org

--- eoxserver/core/views.py
+++ eoxserver/core/views.py
@@ -1,3 +1,28 @@
+#-----------------------------------------------------------------------
+# $Id$
+#
+# This software is named EOxServer, a server for Earth Observation data.
+#
+# Copyright (C) 2011 EOX IT Services GmbH
+# Authors: Stephan Krause, Stephan Meissl
+#
+# This file is part of EOxServer <http://eoxserver.org>.
+#
+#    EOxServer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License,
+#    or (at your option) any later version.
+#
+#    EOxServer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with EOxServer. If not, see <http://www.gnu.org/licenses/>.
+#
+#-----------------------------------------------------------------------
+
 from django.shotcuts import render_to_response
 
 def index(request):
