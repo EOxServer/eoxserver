0730b417449ba20aec5613b107672099558f88f6
Stephan Meißl <stephan.meissl@eox.at>
2011-06-06 10:15:27 +0000

    Adjusting EOxServer URL to http://eoxserver.org
    git-svn-id: http://eoxserver.org/svn/trunk@238 2d754ebb-3ea1-4027-b3e0-ce82c6c57d3b

--- eoxserver/resources/coverages/rangetype.py
+++ eoxserver/resources/coverages/rangetype.py
@@ -6,7 +6,7 @@
 # Copyright (C) 2011 EOX IT Services GmbH
 # Authors: Stephan Krause, Stephan Meissl
 #
-# This file is part of EOxServer <http://www.eoxserver.org>.
+# This file is part of EOxServer <http://eoxserver.org>.
 #
 #    EOxServer is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published
