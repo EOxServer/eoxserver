6885d9793d6c148d66cd2a807cd3adeff181f35e
Stephan Meißl <stephan.meissl@eox.at>
2011-06-16 11:00:21 +0000

    Adjusting EO-WCS Profile elements to RFC version 0.4.0 (see also #33).

--- eoxserver/services/ows/wcs/encoders.py
+++ eoxserver/services/ows/wcs/encoders.py
@@ -429,8 +429,8 @@ class WCS20EOAPEncoder(WCS20Encoder):
         ])
 
     def encodeEOProfiles(self):
-        return [self._makeElement("ows", "Profile", "http://www.opengis.net/spec/WCS_profile_earth-observation/1.0/conf/ap-eo"),
-                self._makeElement("ows", "Profile", "http://www.opengis.net/spec/WCS_profile_earth-observation/1.0/conf/ap-eo_get-kvp")]
+        return [self._makeElement("ows", "Profile", "http://www.opengis.net/spec/WCS_application-profile_earth-observation/1.0/conf/eowcs"),
+                self._makeElement("ows", "Profile", "http://www.opengis.net/spec/WCS_application-profile_earth-observation/1.0/conf/eowcs_get-kvp")]
 
     def encodeDescribeEOCoverageSetOperation(self, http_service_url):
         return self._makeElement("ows", "Operation", [
