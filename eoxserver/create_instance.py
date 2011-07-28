b226b7c32ae1d439f20936a53cbf6ca289fc5c65
Fabian Schindler <fabian.schindler@eox.at>
2011-07-28 16:39:27 +0000

    Fixed requirement depicted with ticket #55. The `argparse` module is still tried to be used, only as a fallback `optparse` is imported. To improve the interface to the Django administration facilities, the `os.system` call for the `django-admin` script is replaced by a direct call to the according Python function.

--- eoxserver/create_instance.py
+++ eoxserver/create_instance.py
@@ -34,17 +34,94 @@ Create a new EOxServer instance. This instance will create a root directory
 with the instance name in the given (optional) directory.
 """
 
-import argparse
 import tempfile
 import shutil
 import os
 import sys
+import django.core.management
 
 # tags to be replaced in the template files
 TAG_PATH_SRC = "<$PATH_SRC$>"
 TAG_PATH_DST = "<$PATH_DST$>"
 TAG_INSTANCE_ID = "<$INSTANCE_ID$>"
 TAG_MAPSCRIPT = "<$MAPSCRIPT_PATH$>"
+
+def parse_arguments(argv):
+    # set up a parser for command line arguments
+    description = """Create a new EOxServer instance. This instance 
+                  will create a root directory with the instance name in
+                  the given (optional) directory.
+
+                  If the --init_spatialite flag is set, then the initial
+                  database will be created and initialized.
+                  """
+    options = [(
+            ['-d', '--dir'], {
+                'default': '.',
+                'help': 'Optional base directory. Defaults to the current directory.'
+            }
+        ), (
+            ['--initial_data'], {
+                'metavar': 'DIR',
+                'default': False,
+                'help': 'Location of the initial data. Must be JSON.'
+            }
+        ), (
+            ['--init_spatialite'], {
+                'action': 'store_true',
+                'help': 'Flag to initialize the sqlite database.'
+            }
+        ), (
+            ['--mapscript-dir'], {
+                'default': False,
+                'metavar': 'DIR',
+                'help': 'Optional path to the MapServer mapscript library.'
+            }
+        ), (
+            ['instance_id'], {
+                'nargs': 1,
+                'action': 'store',
+                'metavar': 'INSTANCE_ID',
+                'help': 'Mandatory name of the eoxserver instance.'
+            }
+        )
+    ]
+    
+    try:
+        # first try with argparse
+        import argparseX
+        parser = argparse.ArgumentParser()
+        parser.description = description
+
+        for largs, kwargs in options:
+            parser.add_argument(*largs, **kwargs)
+
+        args = parser.parse_args(argv)
+        args.instance_id = args.instance_id[0]
+        
+    except ImportError:
+        # Fallback if argparse is not available
+        import optparse
+        
+        parser = optparse.OptionParser()
+        parser.description = " ".join([line.strip() for line in description.split("\n")])
+
+        for largs, kwargs in options:
+            try:
+                parser.add_option(*largs, **kwargs)
+            except optparse.OptionError: pass
+
+        parser.usage = "Usage: %s [options] INSTANCE_ID" % sys.argv[0]
+        parser.epilog = "  INSTANCE_ID          Mandatory name of the eoxserver instance."
+        
+        args, positional_args = parser.parse_args(argv)
+        
+        if len(positional_args) != 1:
+            parser.error("Exactly one instance name is expected.")
+
+        args.instance_id = positional_args[0]
+
+    return args
 
 def copy_and_replace_tags(src_pth, dst_pth, replacements={}):
     """Helper function to copy a file and replace tags within a file."""
@@ -64,35 +141,12 @@ def create_file(dir_or_path, file=None):
     f = open(dir_or_path, 'w')
     f.close()
 
-def run(args):
-    # set up a parser for command line arguments
-    parser = argparse.ArgumentParser()
-    parser.description = """Create a new EOxServer instance. This instance 
-                         will create a root directory with the instance name in
-                         the given (optional) directory.
+def run(argv):
+    # parse command line arguments
+    args = parse_arguments(argv)
 
-                         If the --init_spatialite flag is set, then the initial
-                         database will be created and initialized.
-                         """
-
-    # set up possible command line arguments
-    parser.add_argument('-d', '--dir', default='.',
-                        help='Optional base directory. Defaults to the current directory.')
-    parser.add_argument('--initial_data', metavar='DIR', default=False,
-                        help='Location of the initial data. Must be JSON.')
-    parser.add_argument('--init_spatialite', action='store_true',
-                        help='Flag to initialize the sqlite database.')
-    parser.add_argument('--mapscript-dir', default=False,
-                        metavar='DIR',
-                        help='Optional path to the MapServer mapscript library.')
-    parser.add_argument('instance_id', nargs=1, action='store',
-                        metavar='INSTANCE_ID',
-                        help='Mandatory name of the eoxserver instance.')
+    instance_id = args.instance_id
     
-    args = parser.parse_args(args)
-
-    
-    instance_id = args.instance_id[0]
     dst_root_dir = os.path.abspath(args.dir)
     dst_inst_dir = os.path.join(dst_root_dir, instance_id)
     dst_conf_dir = os.path.join(dst_inst_dir, "conf")
@@ -106,14 +160,11 @@ def run(args):
     if args.initial_data:
         initial_data = os.path.abspath(args.initial_data)
 
-    # run django-admin.py startproject
-
     os.chdir(dst_root_dir)
 
-    print("Initializing django project.\n\n")
-    print("Please ignore error the following line if it contains a message like 'sh: django-admin.py: not found'.")
-    assert((os.system("django-admin.py startproject %s" % instance_id) == 0) or
-           (os.system("django-admin startproject %s" % instance_id) == 0))
+    # create the initial django folder structure
+    print("Initializing django project folder.")
+    django.core.management.call_command("startproject", instance_id)
 
     # create the `conf`, `data`, `logs` and `fixtures` subdirectories
     os.mkdir(dst_conf_dir)
@@ -156,17 +207,15 @@ def run(args):
         # initialize the spatialite database file
         os.chdir(dst_data_dir)
         db_name = "conf.sqlite"
-        init_sql_path = os.path.join(src_conf_dir, "init_spatialite-2.3.sql")
         print("Setting up initial database.")
         try:
             from pyspatialite import dbapi2 as db
             conn = db.connect(db_name)
-            #f = open(init_sql_path)
-            #sql = f.read()
             conn.execute("SELECT InitSpatialMetadata()")
             conn.commit()
             conn.close()
         except ImportError:
+            init_sql_path = os.path.join(src_conf_dir, "init_spatialite-2.3.sql")
             os.system("spatialite conf.sqlite < %s" % init_sql_path)
 
 if __name__ == "__main__":
