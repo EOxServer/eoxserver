bb95f2168e526ae1a1b6d6986ab372976e0f7dc4
Stephan Meißl <stephan.meissl@eox.at>
2011-08-10 17:29:56 +0000

    Adjusting names in admin. Adding media files and URLs for development.

--- eoxserver/conf/TEMPLATE_settings.py
+++ eoxserver/conf/TEMPLATE_settings.py
@@ -85,12 +85,12 @@ USE_L10N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = join('<$PATH_SRC$>', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = 'eoxserver_media'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
