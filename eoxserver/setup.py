ec0a3dcad98bcc194f87e94b935163db3c304a30
Fabian Schindler <fabian.schindler@eox.at>
2011-10-24 11:55:05 +0000

    Added minimal setup script.

---
+++ eoxserver/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'EOxServer',
+    version = '1.0',
+    #packages = find_packages(),  # include all packages under src
+    #package_dir = {'':'src'},   # tell distutils packages are under src
+)
