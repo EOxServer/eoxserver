e2fd8d729302f5b406954f5fc3800b16bc51ab46
Fabian Schindler <fabian.schindler@eox.at>
2011-11-07 18:43:12 +0000

    Added 'eoxserver-admin.py' as a script location for the setup module.

--- eoxserver/setup.py
+++ eoxserver/setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 setup(
     name = 'EOxServer',
     version = '1.0',
+    scripts = ["scripts/eoxserver-admin.py"],
     #packages = find_packages(),  # include all packages under src
     #package_dir = {'':'src'},   # tell distutils packages are under src
 )
