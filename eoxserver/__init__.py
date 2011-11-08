2f1f42eb8d1da5703fef8e74f96d004a9146655d
Fabian Schindler <fabian.schindler@eox.at>
2011-11-08 16:35:31 +0000

    Moved setup.py to correct location. Improved setup script to contain more correct metadata to be actually executed. Added version information for eoxserver.
    git-svn-id: http://eoxserver.org/svn/trunk@820 2d754ebb-3ea1-4027-b3e0-ce82c6c57d3b

---
+++ eoxserver/__init__.py
@@ -0,0 +1,19 @@
+from django.utils.version import get_svn_revision
+
+VERSION = (0, 2, 0, 'alpha', 0)
+SVN_REV = '$Id$'
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3:] == ('alpha', 0):
+        version = '%s pre-alpha' % version
+    else:
+        if VERSION[3] != 'final':
+            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+    
+    svn_rev = get_svn_revision(".")
+    if svn_rev != u'SVN-unknown':
+        version = "%s %s" % (version, svn_rev)
+    return version
