36022ffaf301baf7534da47117f3e901b39f2bde
Stephan Meißl <stephan.meissl@eox.at>
2011-11-10 10:55:55 +0000

    Using string "dev" in development releases.
    git-svn-id: http://eoxserver.org/svn/trunk@829 2d754ebb-3ea1-4027-b3e0-ce82c6c57d3b

--- eoxserver/__init__.py
+++ eoxserver/__init__.py
@@ -37,8 +37,8 @@ def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
+    if VERSION[3:] == ('dev', 0):
+        version = '%s dev' % version
     else:
         if VERSION[3] != 'final':
             version = '%s %s %s' % (version, VERSION[3], VERSION[4])
