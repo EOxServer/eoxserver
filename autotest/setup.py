596f64d1e579317599ab4af9afbc3db07beee804
Stephan Meißl <stephan.meissl@eox.at>
2011-12-06 15:10:33 +0000

    Extend setup.py and MANIFEST. Either non-empty long_description or url is needed in setup.py.

--- autotest/setup.py
+++ autotest/setup.py
@@ -1,5 +1,38 @@
+#-------------------------------------------------------------------------------
+# $Id$
+#
+# Project: EOxServer <http://eoxserver.org>
+# Authors: Stephan Krause <stephan.krause@eox.at>
+#          Stephan Meissl <stephan.meissl@eox.at>
+#          Fabian Schindler <fabian.schindler@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2011 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
 import os
 
+# Hack to remove setuptools "feature" which resulted in
+# ignoring MANIFEST.in when code is in an svn repository.
+# TODO find a nicer solution
 from setuptools.command import sdist
 del sdist.finders[:]
 
@@ -30,9 +63,15 @@ setup(
     # Metadata
     author="EOX IT Services GmbH",
     author_email="office@eox.at",
+    maintainer="EOX IT Services GmbH",
+    maintainer_email="packages@eox.at",
     
-    description="",
-    long_description="",
-
+    description="EOxServer is a server for Earth Observation (EO) data",
+    long_description=read("README"),
+    
+    license="EOxServer Open License (MIT-style)",
+    keywords="Earth Observation, EO, OGC, WCS, WMS",
+    url="http://eoxserver.org/"
+    
     cmdclass={'install': install},
 )
