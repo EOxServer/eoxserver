97380bd9a6ea7ae10d5674aaa653dd90e190c3c2
Martin Paces <martin.paces@eox.at>
2011-12-27 14:15:56 +0000

    WCS-T: sandbox to trunk merger

---
+++ eoxserver/resources/processes/models.py
@@ -0,0 +1,184 @@
+#-----------------------------------------------------------------------
+# $Id$
+#
+# Description: 
+#
+#  processes - django DB model 
+#
+#-------------------------------------------------------------------------------
+#
+# Project: EOxServer <http://eoxserver.org>
+# Authors: Martin Paces <martin.paces@iguassu.cz>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2011 Iguassu Software Systems, a.s 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+from django.db import models
+
+#-------------------------------------------------------------------------------
+
+# status to text conversion and back 
+STATUS2TEXT  = { 0:"UNDEFINED", 1:"ACCEPTED", 2:"SCHEDULED", 3:"RUNNING", 4:"PAUSED", 5:"FINISHED", 6:"FAILED" } 
+STATUS2COLOR = { 0:"mangenta", 1:"brown", 2:"darkcyan", 3:"green", 4:"maroon", 5:"blue", 6:"red" } 
+TEXT2STATUS  = dict( map( lambda item : ( item[1] , item[0] ) , STATUS2TEXT.items() ) )
+
+#-------------------------------------------------------------------------------
+
+class Type( models.Model ) : 
+    """ 
+        Process Type (aka Class): 
+            identifier   - process class ID 
+            handler      - python dot path to handler function 
+            timeout      - time in second after which the unfinished process is 
+                           considered to be abandoned and it is restarted 
+                           (number of restarts is limited by maxstart, default timeout is 3600 s) 
+            timeret      - retention time - period of time to keep finished processes
+                           in case of zero or negative value the results will be kept forever 
+                           (default is -1) 
+            maxstart     - max. number of attemps to execute the task (first run and possible restarts)
+                           When the number of (re)starts is exceeded the task is marked as failed 
+                           and rejected from further processing. (default is 3)
+                           
+            ...  some human-friendly meta-data may be added in future  
+        
+    """
+    identifier = models.CharField( max_length=64   , unique=True , blank=False , null=False, editable = False )
+    handler    = models.CharField( max_length=1024 , blank=False , null=False, editable = False )
+    maxstart   = models.IntegerField( default = 3 , blank = False , null = False , editable = False ) 
+    timeout    = models.FloatField( default = 3600.0 , blank = False , null = False , editable = False ) 
+    timeret    = models.FloatField( default = -1.0 , blank = False , null = False , editable = False ) 
+
+    def __unicode__( self ) :
+        return unicode( self.identifier ) 
+
+    def __str__( self ) : return unicode(self).encode("utf8")
+
+    class Admin : pass
+
+
+class Instance( models.Model ) : 
+    """ 
+        Process Type (aka Class): 
+            type         - process class of this instance 
+            identifier   - process instance ID 
+            status       - current status of the process 
+            timeInsert   - instance creation time (aka enqueue or insert time ) 
+            timeUpdate   - instance last status update time 
+    """
+    type        = models.ForeignKey( Type , blank=False , null=False , editable = False )
+    identifier  = models.CharField( max_length=64 , blank=False , null=False, editable = False )
+    timeInsert  = models.DateTimeField( auto_now_add=True , editable = False )
+    timeUpdate  = models.DateTimeField( auto_now=True , editable = False )
+    status      = models.IntegerField( null=False , editable = False )
+
+    class Meta: unique_together = (('identifier','type'),)
+
+    def __unicode__( self ) :
+        return u"%s::%s"%( self.type.identifier , self.identifier ) 
+
+    def __str__( self ) : return unicode(self).encode("utf8")
+
+    class Admin : pass
+
+
+class Task( models.Model ): 
+    """ 
+        Process Task Queue:
+
+            instance   - process instance 
+    """
+    instance     = models.ForeignKey( Instance , blank=False , null=False , editable = False )
+    time         = models.DateTimeField( auto_now=True, editable = False )
+    lock         = models.BigIntegerField( default = 0 )  
+
+    def __unicode__( self ) :
+        return u"%s::%s"%( self.instance.type.identifier , self.instance.identifier ) 
+
+    def __str__( self ) : return unicode(self).encode("utf8")
+
+    class Admin : pass
+
+
+class LogRecord( models.Model ): 
+    """ 
+        Process Status Log:
+
+            instance   - process instance 
+            time       - time-stamp of the log reccord 
+            status     - status code of the process instance 
+            message    - text message associated to the log message 
+    """
+    instance     = models.ForeignKey( Instance , blank=False , null=False , editable = False )
+    time         = models.DateTimeField( auto_now=True, editable = False )
+    status       = models.IntegerField( null=False , choices = STATUS2TEXT.items(), editable = False )
+    message      = models.TextField( editable = False )
+
+    # pid - local process ID 
+
+    def __unicode__( self ) :
+
+        message = unicode(self.message) 
+        if ( 28 < len(message) ) : 
+            message = u"%s ..."%message[:28] 
+
+        return u'%22.22s [%s] %s\t%s' % ( self.time , self.instance , STATUS2TEXT[self.status] , message ) 
+
+    def __str__( self ) : return unicode(self).encode("utf8")
+ 
+    class Admin : pass
+
+
+class Response( models.Model ): 
+    """ 
+        Process Response Store: 
+
+            instance   - process instance 
+            response   - process XML response (if not in plaintext GZIP+BASE64 is applied) 
+    """
+    instance    = models.ForeignKey( Instance , blank=False , null=False , editable = False , unique = True )
+    response    = models.TextField( editable = False )
+
+    def __unicode__( self ) : return unicode( self.instance )  
+
+    def __str__( self ) : return unicode(self).encode("utf8")
+
+    class Admin : pass
+
+
+class Inputs( models.Model ): 
+    """ 
+        Process Input Store 
+
+            instance   - process instance 
+            input      - task inputs 
+    """
+
+    instance    = models.ForeignKey( Instance , blank=False , null=False , editable = False , unique = True )
+    inputs      = models.TextField( editable = False ) # store the data as Base64 encoded pickle object
+
+    def __unicode__( self ) : return unicode( self.instance )  
+
+    def __str__( self ) : return unicode(self).encode("utf8")
+
+    class Admin : pass 
+
+
---
+++ eoxserver/resources/processes/views.py
@@ -0,0 +1,145 @@
+#-----------------------------------------------------------------------
+# $Id$
+#
+# Description: 
+#
+#  views 
+#
+#-------------------------------------------------------------------------------
+#
+# Project: EOxServer <http://eoxserver.org>
+# Authors: Martin Paces <martin.paces@iguassu.cz>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2011 Iguassu Software Systems, a.s 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+from django.http import HttpResponse
+
+from eoxserver.resources.processes.models import Type,Instance, LogRecord  
+from eoxserver.resources.processes.tracker import TaskStatus, getTaskResponse
+from eoxserver.resources.processes.models import STATUS2TEXT , STATUS2COLOR , TEXT2STATUS 
+
+
+STATUS2CTEXT = {} 
+
+for k in STATUS2TEXT : 
+    STATUS2CTEXT[k] = '<span style="color:%s">%s</span>' % ( STATUS2COLOR[k] , STATUS2TEXT[k] ) 
+
+def task(request): 
+
+    status = request.GET.get('status',None) 
+    rtype  = request.GET.get('type',None) 
+
+    path = request.path.rpartition("/")[0]
+
+    _filter = {} 
+    _subtitle = "All Tasks"
+
+    if status or rtype : 
+        _subtitle = "Tasks Having:"
+
+    if status : 
+        status = str(status).upper() 
+        _filter['status'] = TEXT2STATUS[status]
+        _subtitle += " status=%s " % status 
+
+    if rtype : 
+        _filter['type'] = Type.objects.get(identifier=rtype) 
+        _subtitle += " type=%s " % rtype
+
+    r = [] 
+    r.append('<html>')
+    r.append('<head><title>Task Status</title></head>')
+    r.append('<meta http-equiv="refresh" content="15">') 
+    r.append('<body style="font-family:sans-serif">') 
+    r.append('<table border="1" padding="2">') 
+    r.append('<h2>Task Status</h2>')
+    if _subtitle : r.append( '<h3>%s</h3>'%_subtitle )
+    r.append('<tr style="font-style:italic;font-weight:bold"><td>Update Time</td><td>Insert Time</td><td>Request Type</td><td>Request ID</td><td>Status</td><td>Info</td></tr>')
+
+    for item in Instance.objects.filter(**_filter).select_related().order_by("-timeUpdate") :
+        status = '<a href="%s/task?status=%s">%s</a>'%( path , STATUS2TEXT[item.status] , STATUS2CTEXT[item.status] ) 
+        rtype0 = item.type.identifier 
+        rtype  = '<a href="%s/task?type=%s" style="color:black">%s</a>'%( path , rtype0 , rtype0 ) 
+        rid    = '<a href="%s/status/%s/%s" style="color:black">%s</a>' % ( path , rtype0 , item.identifier , item.identifier ) 
+        try : 
+            info = "enqueued (%s)"%str(item.task_set.get().lock) 
+        except : 
+            if item.status in ( TaskStatus.FINISHED , TaskStatus.FAILED ) :
+                info = '<a href="%s/response/%s/%s" style="color:black">response</a>' % ( path , rtype0 , item.identifier ) 
+            else: 
+                info = "&nbsp;"
+        r.append('<tr><td>%19.19s</td><td>%19.19s</td><td>%s</td><td style="font-family:monospace;font-size:125%%">%s</td><td>%s</td></td><td>%s</td></tr>'% \
+            (item.timeUpdate,item.timeInsert,rtype,rid,status,info) )
+# 
+    r.append('</table>') 
+    r.append('</body>')
+    r.append('</html>') 
+    
+    return HttpResponse("\n".join(r),content_type='text/html')
+
+
+def status(request,requestType=None,requestID=None):
+
+    _filter   = {} 
+    _subtitle = "All Tasks' History "
+
+    if ( requestType is not None ) and ( requestID is not None ) : 
+        _filter["instance"] = Type.objects.get(identifier=requestType).instance_set.get(identifier=requestID)
+        _subtitle = "Single Task's History"
+        
+    path = request.path.rpartition("/status")[0]
+
+    r = [] 
+    r.append('<html>')
+    r.append('<head><title>Task Status Log</title></head>')
+    r.append('<meta http-equiv="refresh" content="15">') 
+    r.append('<body style="font-family:sans-serif">') 
+    r.append('<table border="1" padding="2">') 
+    r.append('<h2>Process Status Log</h2>')
+    if _subtitle : r.append( '<h3>%s</h3>'%_subtitle )
+    r.append('<tr style="font-style:italic;font-weight:bold"><td>Time</td><td>RequestID</td><td>Status</td><td>Message</td><td>Response</td></tr>')
+
+    for item in LogRecord.objects.filter(**_filter).select_related().order_by("-time") :
+        status = STATUS2CTEXT[item.status] 
+        rtype  = item.instance.type.identifier
+        rid    = '<a href="%s/status/%s/%s">%s</a>' % ( path , rtype , item.instance.identifier , item.instance.identifier ) 
+        response = "" 
+        if ( item.status in ( TaskStatus.FINISHED , TaskStatus.FAILED ) ) : 
+            response = '<a href="%s/response/%s/%s">view</a>' % ( path , rtype , item.instance.identifier ) 
+
+        r.append('<tr><td>%19.19s</td><td style="font-family:monospace;font-size:125%%">%s</td><td>%s</td><td>%s</td><td>%s<td/></tr>'% \
+            (item.time,rid,status,item.message,response) )
+    r.append('</table>') 
+    r.append('</body>')
+    r.append('</html>') 
+
+    return HttpResponse("\n".join(r),content_type='text/html')
+
+
+def response(request,requestType,requestID): 
+
+    try : 
+        return HttpResponse( getTaskResponse( requestType , requestID ) , content_type='text/xml' ) 
+    except Exception as e : 
+        return HttpResponse('ERROR: Response not available! requestClass="%s" ; requestID = "%s" '%\
+                    (requestType,requestID),status=500,content_type='text/plain') 
