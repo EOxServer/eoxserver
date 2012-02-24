5eed3d6b09a648fed7bc2fa9cd827985ccd9bbc0
Stephan Meißl <stephan.meissl@eox.at>
2012-02-24 20:28:51 +0000

    Adding version examples.

--- eoxserver/__init__.py
+++ eoxserver/__init__.py
@@ -31,6 +31,10 @@
 from django.utils.version import get_svn_revision
 
 VERSION = (0, 2, 0, 'dev', 0)
+#VERSION = (0, 2, 0, 'alpha', 1)
+#VERSION = (0, 2, 0, 'beta', 1)
+#VERSION = (0, 2, 0, 'rc', 1)
+#VERSION = (0, 2, 0, 'final', 0)
 SVN_REV = '$Id$'
 
 def get_version():
