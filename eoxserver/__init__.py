fc7fcc2dcb59c7c0eacc25b6856de3f799648f10
Stephan Meißl <stephan.meissl@eox.at>
2012-02-26 01:01:14 +0000

    Correcting error in previous commit.
    git-svn-id: http://eoxserver.org/svn/trunk@1341 2d754ebb-3ea1-4027-b3e0-ce82c6c57d3b

--- eoxserver/__init__.py
+++ eoxserver/__init__.py
@@ -30,7 +30,7 @@
 
 from django.utils.version import get_svn_revision
 
-VERSION = (0, 2, 1, 'dev', 0)
+VERSION = (0, 2, 0, 'dev', 0)
 #VERSION = (0, 2, 0, 'alpha', 1)
 #VERSION = (0, 2, 0, 'beta', 1)
 #VERSION = (0, 2, 0, 'rc', 1)
