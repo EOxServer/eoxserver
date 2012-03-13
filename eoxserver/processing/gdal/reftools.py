70ddbfa19ec820099e89c8390dd26543ecb94453
Fabian Schindler <fabian.schindler@eox.at>
2012-03-13 14:41:59 +0000

    All reftool functions can now be called with either a path to a dataset or a gdal.Dataset object itself.
    git-svn-id: http://eoxserver.org/svn/trunk@1514 2d754ebb-3ea1-4027-b3e0-ce82c6c57d3b

--- eoxserver/processing/gdal/reftools.py
+++ eoxserver/processing/gdal/reftools.py
@@ -1,8 +1,9 @@
 from tempfile import mkstemp
 import ctypes as C
 import os.path
-
 import logging
+
+from osgeo import gdal
 
 from eoxserver.core.system import System
 from eoxserver.core.exceptions import InternalError
@@ -31,15 +32,15 @@ try:
     _lib = C.LibraryLoader(C.CDLL).LoadLibrary(_lib_path)
 
     _get_footprint_wkt = _lib.eoxs_get_footprint_wkt
-    _get_footprint_wkt.argtypes = [C.c_char_p]
+    _get_footprint_wkt.argtypes = [C.c_void_p]
     _get_footprint_wkt.restype = C.POINTER(C.c_char)
 
     _rect_from_subset = _lib.eoxs_rect_from_subset
-    _rect_from_subset.argtypes = [C.c_char_p, C.POINTER(SUBSET)]
+    _rect_from_subset.argtypes = [C.c_void_p, C.POINTER(SUBSET)]
     _rect_from_subset.restype = C.POINTER(RECT)
 
     _create_rectified_vrt = _lib.eoxs_create_rectified_vrt
-    _create_rectified_vrt.argtypes = [C.c_char_p, C.c_char_p, C.c_int]
+    _create_rectified_vrt.argtypes = [C.c_void_p, C.c_char_p, C.c_int]
     _create_rectified_vrt.restype = C.c_int
 
     _free_string = _lib.eoxs_free_string
@@ -52,23 +53,34 @@ except:
     REFTOOLS_USABLE = False
 
 
-def get_footprint_wkt(path):
+def _open_ds(path_or_ds):
+    if isinstance(path_or_ds, gdal.Dataset):
+        return path_or_ds
+    gdal.AllRegister()
+    return gdal.Open(path_or_ds)
+
+
+def get_footprint_wkt(path_or_ds):
     if not REFTOOLS_USABLE:
         raise InternalError("Referenceable grid handling disabled")
-        
-    ret = _get_footprint_wkt(path)
+    
+    ds = _open_ds(path_or_ds)
+    
+    ret = _get_footprint_wkt(C.c_void_p(long(ds.this)))
     string = C.cast(ret, C.c_char_p).value
     
     _free_string(ret)
     return string
 
-def rect_from_subset(path, srid, minx, miny, maxx, maxy):
+def rect_from_subset(path_or_ds, srid, minx, miny, maxx, maxy):
     if not REFTOOLS_USABLE:
         raise InternalError("Referenceable grid handling disabled")
 
+    ds = _open_ds(path_or_ds)
+    
     rect = RECT()
     ret = _rect_from_subset(
-        path,
+        C.c_void_p(long(ds.this)),
         C.byref(SUBSET(srid, minx, miny, maxx, maxy)),
         C.byref(rect)
     )
@@ -77,21 +89,24 @@ def rect_from_subset(path, srid, minx, miny, maxx, maxy):
     
     return (rect.x_off, rect.y_off, rect.x_size, rect.y_size)
 
-def create_rectified_vrt(path, vrt_path, srid=None):
+def create_rectified_vrt(path_or_ds, vrt_path, srid=None):
     if not REFTOOLS_USABLE:
         raise InternalError("Referenceable grid handling disabled")
 
+    ds = _open_ds(path_or_ds)
+    ptr = C.c_void_p(long(ds.this))
+
     if srid:
-        ret = _create_rectified_vrt(path, vrt_path, srid)
+        ret = _create_rectified_vrt(ptr, vrt_path, srid)
     else:
-        ret = _create_rectified_vrt(path, vrt_path, 0)  
+        ret = _create_rectified_vrt(ptr, vrt_path, 0)  
     
     if not ret:
         raise InternalError(
             "Could not create rectified VRT."
         )
 
-def create_temporary_vrt(path, srid=None):
+def create_temporary_vrt(path_or_ds, srid=None):
     if not REFTOOLS_USABLE:
         raise InternalError("Referenceable grid handling disabled")
 
@@ -100,6 +115,6 @@ def create_temporary_vrt(path, srid=None):
         suffix = ".vrt"
     )
     
-    create_rectified_vrt(path, vrt_path, srid)
+    create_rectified_vrt(path_or_ds, vrt_path, srid)
     
     return vrt_path
