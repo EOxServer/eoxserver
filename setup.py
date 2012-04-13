d7c811640a745962bbbddeef7805cccc3839d548
Stephan Meißl <stephan.meissl@eox.at>
2012-04-13 17:35:34 +0000

    Adding atpd and validate_xml to scripts after renaming.

--- setup.py
+++ setup.py
@@ -95,7 +95,11 @@ setup(
     packages=packages,
     data_files=data_files,
     include_package_data=True,
-    scripts=["eoxserver/scripts/eoxserver-admin.py"],
+    scripts=[
+        "eoxserver/scripts/eoxserver-admin.py",
+        "tools/eoxserver-atpd.py",
+        "tools/eoxserver-validate_xml.py"
+    ],
     
     ext_modules=[
         Extension(
