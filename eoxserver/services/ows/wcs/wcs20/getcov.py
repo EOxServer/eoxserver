2f1b1708525fabf808a8a58d7ce3a2688d7a11d9
Martin Paces <martin.paces@eox.at>
2012-08-06 16:33:21 +0000

    bugfix: #156 Referenceable DS multipart response - rangeSet reference.
    git-svn-id: http://eoxserver.org/svn/trunk@1931 2d754ebb-3ea1-4027-b3e0-ce82c6c57d3b

--- eoxserver/services/ows/wcs/wcs20/getcov.py
+++ eoxserver/services/ows/wcs/wcs20/getcov.py
@@ -328,7 +328,8 @@ class WCS20GetReferenceableCoverageHandler(BaseRequestHandler):
                     _subset = ( subset.crs_id, bbox.size,
                         (subset.minx, subset.miny, subset.maxx, subset.maxy), footprint ) 
 
-            cov_desc_el = encoder.encodeReferenceableDataset(coverage,reference,format.mimeType,True,_subset)
+            cov_desc_el = encoder.encodeReferenceableDataset(coverage,
+                            "cid:%s"%reference,mime_type,True,_subset)
             
             # NOTE: the multipart subtype will be the same as the one requested 
             resp = self._get_multipart_response( data, format.mimeType, filename, reference,
