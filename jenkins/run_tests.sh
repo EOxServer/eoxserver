03f8497d42eb52d21a6a9f719fd27ad4f8552a41
Stephan Meißl <stephan.meissl@eox.at>
2013-03-08 17:20:01 +0000

    Adjusting relative paths in test data and execution. Need to find better handling (#171, #206).

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -4,13 +4,26 @@
 cd $WORKSPACE
 source .venv/bin/activate
 
-cd autotest
+cd autotest/autotest
 export XML_CATALOG_FILES="$WORKSPACE/schemas/catalog.xml"
 echo "**> running tests ..."
-python manage.py test services -v2
+python ../manage.py test services -v2
+#TODO: Enable testing of all apps
 #python manage.py test core services coverages -v2
 
 # Run command line tests
 echo "**> running command line tests ..."
 python manage.py syncdb --noinput
 python manage.py loaddata auth_data.json initial_rangetypes.json
+#TODO: Expand this tests:
+#eoxs_add_dataset_series
+#eoxs_register_dataset
+#eoxs_insert_into_series
+#eoxs_check_id
+#eoxs_list_ids
+#eoxs_load_rangetypes
+#eoxs_list_rangetypes
+#eoxs_synchronize
+#eoxs_remove_from_series
+#eoxs_deregister_dataset
+#compare with expected
