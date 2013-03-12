eecf76f513077c91e1755f6cd01ada44db489458
Marko Locher <marko.locher@eox.at>
2013-03-12 07:22:41 +0000

    Backported r2303 and r2305 to 0.2 branch of EOxServer Added option for django1.3

--- jenkins/prepare_instance.sh
+++ jenkins/prepare_instance.sh
@@ -14,6 +14,26 @@ fi
 # activate the virtual environment
 source .venv/bin/activate
 
+# Install the required Django version
+case $DJANGO in
+	"django1.3")
+		echo "Using latest django 1.3"
+		pip install "django<1.4,>=1.3"
+		;;
+	"django1.4")
+		echo "Using latest django 1.4"
+		pip install "django<1.5,>=1.4"
+		;;
+	"django1.5")
+		echo "Using latest django 1.5"
+		pip install "django<1.6,>=1.5"
+		;;
+	*)
+		echo "Unknown django version, Exiting..."
+		exit 1
+		;;
+esac
+
 # Install EOxServer
 echo "**> installing eoxserver..."
 python setup.py develop
@@ -40,3 +60,24 @@ sed -e 's/allowed_actions=/allowed_actions=Add,Delete/' -i autotest/conf/eoxserv
 if [ $OS != 'Ubuntu' ]; then
   sed -e 's/#binary_raster_comparison_enabled=false/binary_raster_comparison_enabled=false/' -i autotest/conf/eoxserver.conf
 fi
+
+# Configure the specified database system
+case $DB in
+	"spatialite")
+		echo "Using spatialite database!"
+		# Noting to do, as by default eoxserver autotest will use a spatialite db
+		;;
+	"postgis")
+		echo "Using postgis database!"
+		sed -e "s/'ENGINE':.*/'ENGINE': 'django.contrib.gis.db.backends.postgis',/" -i autotest/settings.py
+		sed -e "s/'NAME':.*/'NAME': 'eoxserver_testing',/" -i autotest/settings.py
+		sed -e "s/'USER':.*/'USER': 'jenkins',/" -i autotest/settings.py
+		sed -e "s/'PASSWORD':.*/'PASSWORD': 'eeJ0Kain',/" -i autotest/settings.py
+		sed -e "/'HOST':.*/d" -i autotest/settings.py
+		sed -e "/'PORT':.*/d" -i autotest/settings.py
+		;;
+	*)
+		echo "Unknown database system, Exiting..."
+		exit 1
+		;;
+esac
