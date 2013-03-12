6b8e70e317b519f4802dc58e8ccc5531b50ae905
Stephan Meißl <stephan.meissl@eox.at>
2013-03-12 09:30:58 +0000

    Adding support for Django 1.3 in Jenkins.

--- jenkins/prepare_instance.sh
+++ jenkins/prepare_instance.sh
@@ -5,10 +5,10 @@ OS=`facter operatingsystem`
 # Create the virtual environment if it does not exist
 cd $WORKSPACE
 if [ -d ".venv" ]; then
-  echo "**> virtualenv exists!"
+    echo "**> virtualenv exists!"
 else
-  echo "**> creating virtualenv..."
-  virtualenv --system-site-packages .venv
+    echo "**> creating virtualenv..."
+    virtualenv --system-site-packages .venv
 fi
 
 # activate the virtual environment
@@ -16,22 +16,22 @@ source .venv/bin/activate
 
 # Install the required Django version
 case $DJANGO in
-	"django1.3")
-		echo "Using latest django 1.3"
-		pip install "django<1.4,>=1.3"
-		;;
-	"django1.4")
-		echo "Using latest django 1.4"
-		pip install "django<1.5,>=1.4"
-		;;
-	"django1.5")
-		echo "Using latest django 1.5"
-		pip install "django<1.6,>=1.5"
-		;;
-	*)
-		echo "Unknown django version, Exiting..."
-		exit 1
-		;;
+    "django1.3")
+        echo "Using latest django 1.3"
+        pip install "django<1.4,>=1.3"
+        ;;
+    "django1.4")
+        echo "Using latest django 1.4"
+        pip install "django<1.5,>=1.4"
+        ;;
+    "django1.5")
+        echo "Using latest django 1.5"
+        pip install "django<1.6,>=1.5"
+        ;;
+    *)
+        echo "Unknown django version, Exiting..."
+        exit 1
+        ;;
 esac
 
 # Install EOxServer
@@ -42,16 +42,21 @@ python setup.py develop
 echo "**> creating autotest instance..."
 mv autotest tmp1
 eoxserver-admin.py create_instance autotest --init_spatialite
-mv autotest/manage.py tmp1/
-mv autotest/autotest/ tmp2
-rmdir autotest/
-mv tmp1 autotest
-mv tmp2/settings.py autotest/
-mv tmp2/conf/eoxserver.conf autotest/conf/
-mv tmp2/wsgi.py autotest/
-mv tmp2/data/config.sqlite autotest/data/
-mv tmp2/data/init_spatialite-2.3.sql autotest/data/
-rm -r tmp2
+if [ $DJANGO -eq "django1.3" ]; do
+    cp -R tmp1/* autotest/
+    rm r tmp1
+else
+    mv autotest/manage.py tmp1/
+    mv autotest/autotest/ tmp2
+    rmdir autotest/
+    mv tmp1 autotest
+    mv tmp2/settings.py autotest/
+    mv tmp2/conf/eoxserver.conf autotest/conf/
+    mv tmp2/wsgi.py autotest/
+    mv tmp2/data/config.sqlite autotest/data/
+    mv tmp2/data/init_spatialite-2.3.sql autotest/data/
+    rm -r tmp2
+fi
 sed -e 's/\/autotest\/autotest/\/autotest/' -i autotest/settings.py
 sed -e "s/#'TEST_NAME':/'TEST_NAME':/" -i autotest/settings.py
 sed -e 's/\/autotest\/autotest/\/autotest/' -i autotest/conf/eoxserver.conf
@@ -63,21 +68,21 @@ fi
 
 # Configure the specified database system
 case $DB in
-	"spatialite")
-		echo "Using spatialite database!"
-		# Noting to do, as by default eoxserver autotest will use a spatialite db
-		;;
-	"postgis")
-		echo "Using postgis database!"
-		sed -e "s/'ENGINE':.*/'ENGINE': 'django.contrib.gis.db.backends.postgis',/" -i autotest/settings.py
-		sed -e "s/'NAME':.*/'NAME': 'eoxserver_testing',/" -i autotest/settings.py
-		sed -e "s/'USER':.*/'USER': 'jenkins',/" -i autotest/settings.py
-		sed -e "s/'PASSWORD':.*/'PASSWORD': 'eeJ0Kain',/" -i autotest/settings.py
-		sed -e "/'HOST':.*/d" -i autotest/settings.py
-		sed -e "/'PORT':.*/d" -i autotest/settings.py
-		;;
-	*)
-		echo "Unknown database system, Exiting..."
-		exit 1
-		;;
+    "spatialite")
+        echo "Using spatialite database!"
+        # Noting to do, as by default eoxserver autotest will use a spatialite db
+        ;;
+    "postgis")
+        echo "Using postgis database!"
+        sed -e "s/'ENGINE':.*/'ENGINE': 'django.contrib.gis.db.backends.postgis',/" -i autotest/settings.py
+        sed -e "s/'NAME':.*/'NAME': 'eoxserver_testing',/" -i autotest/settings.py
+        sed -e "s/'USER':.*/'USER': 'jenkins',/" -i autotest/settings.py
+        sed -e "s/'PASSWORD':.*/'PASSWORD': 'eeJ0Kain',/" -i autotest/settings.py
+        sed -e "/'HOST':.*/d" -i autotest/settings.py
+        sed -e "/'PORT':.*/d" -i autotest/settings.py
+        ;;
+    *)
+        echo "Unknown database system, Exiting..."
+        exit 1
+        ;;
 esac
