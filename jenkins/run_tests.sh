2747780f8bdcc6ed6bb16c853b8aa33709f4422c
Stephan Meißl <stephan.meissl@eox.at>
2013-03-15 18:47:41 +0000

    Adjusting jenkins tests.

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -285,9 +285,9 @@ curl -sS -o tmp "http://localhost:8000/ows?service=wcs&request=getcapabilities"
 xmllint --format tmp > tmp1
 curl -sS -o tmp "http://localhost:8000/ows?service=WCS&version=2.0.1&request=GetCapabilities"
 xmllint --format tmp > tmp2
-curl -sS -o tmp "http://localhost:8000/ows?service=WCS&version=2.0.0&request=DescribeCoverage&CoverageId=ENVISAT-MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_uint16_reduced_compressed"
+curl -sS -o tmp "http://localhost:8000/ows?service=WCS&version=2.0.1&request=DescribeCoverage&CoverageId=ENVISAT-MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_uint16_reduced_compressed"
 xmllint --format tmp > tmp3
-curl -sS -o tmp "http://localhost:8000/ows?service=WCS&version=2.0.0&request=DescribeEOCoverageSet&eoId=ENVISAT-MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_uint16_reduced_compressed"
+curl -sS -o tmp "http://localhost:8000/ows?service=WCS&version=2.0.1&request=DescribeEOCoverageSet&eoId=ENVISAT-MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_uint16_reduced_compressed"
 xmllint --format tmp > tmp4
 
 # Restart development server otherwise the GetCoverage requests hangs forever
@@ -296,7 +296,7 @@ python manage.py runserver 1>/dev/null 2>&1 &
 sleep 3
 PID=$!
 
-curl -sS -o tmp "http://localhost:8000/ows?service=wcs&version=2.0.0&request=GetCoverage&CoverageId=ENVISAT-MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_uint16_reduced_compressed&format=image/tiff"
+curl -sS -o tmp "http://localhost:8000/ows?service=wcs&version=2.0.1&request=GetCoverage&CoverageId=ENVISAT-MER_FRS_1PNPDE20060822_092058_000001972050_00308_23408_0077_uint16_reduced_compressed&format=image/tiff"
 
 # Perform binary comparison only on reference platform
 if [ $DB == "spatialite" ]; then
