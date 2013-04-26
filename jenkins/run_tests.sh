d20c5ab3c8bfabd7772817a23e8e655342b18e3b
Stephan Meißl <stephan.meissl@eox.at>
2013-04-26 18:28:04 +0000

    Really enable coverages tests.

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -16,7 +16,7 @@ cd autotest
 ln -s autotest/data/ data
 export XML_CATALOG_FILES="$WORKSPACE/schemas/catalog.xml"
 if [ $OS == "Ubuntu" ]; then
-    python manage.py test services -v2
+    python manage.py test services coverages -v2
 else
     python manage.py test "services|WCS20GetCoverageOutputCRSotherUoMDatasetTestCase,WCS20GetCoverageReprojectedEPSG3857DatasetTestCase,WCS20GetCoverageJPEG2000TestCase" coverages -v2
     # TODO: Make WCS20GetCoverageJPEG2000TestCase work.
