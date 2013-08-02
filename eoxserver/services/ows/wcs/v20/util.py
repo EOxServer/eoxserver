13c93b6b62b4583259972549802b2ca82d3a2126
Fabian Schindler <fabian.schindler@gmx.at>
2013-08-02 18:04:27 +0200

    Moving NameSpace and NameSpaceMap to utils because of importing issues. Final destination still TBD.

--- eoxserver/services/ows/wcs/v20/util.py
+++ eoxserver/services/ows/wcs/v20/util.py
@@ -1,5 +1,31 @@
 from lxml.builder import ElementMaker
-from eoxserver.core.util.xml import NameSpace, NameSpaceMap
+#from eoxserver.core.util.xml import NameSpace, NameSpaceMap
+
+class NameSpace(object):
+    def __init__(self, uri, prefix=None):
+        self._uri = uri
+        self._lxml_uri = "{%s}" % uri
+        self._prefix = prefix
+
+    @property
+    def uri(self):
+        return self._uri
+
+    @property
+    def prefix(self):
+        return self._prefix
+    
+    def __call__(self, tag):
+        return self._lxml_uri + tag
+
+
+class NameSpaceMap(dict):
+    def __init__(self, *namespaces):
+        for namespace in namespaces:
+            self.add(namespace)
+
+    def add(self, namespace):
+        self[namespace.prefix] = namespace.uri
 
 
 # namespace declarations
