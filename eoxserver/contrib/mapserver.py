c00bfe0a9e41a71c493ab87f15ddb4635dd24ce8
Fabian Schindler <fabian.schindler@gmx.at>
2013-09-03 18:27:27 +0200

    Fixing layer and map objects. Adding convenience functions.

--- eoxserver/contrib/mapserver.py
+++ eoxserver/contrib/mapserver.py
@@ -26,13 +26,14 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-
+import time
 import logging
 
-import mapscript
+from mapscript import *
 
 from eoxserver.core.util.multiparttools import mpUnpack, mpPack, capitalize
 from eoxserver.core.util.multiparttools import getMimeType, getMultipartBoundary
+from eoxserver.contrib import gdal
 
 
 logger = logging.getLogger(__name__)
@@ -40,59 +41,19 @@ is_xml = lambda ct: getMimeType(ct) in ("text/xml", "application/xml", "applicat
 is_multipart = lambda ct: getMimeType(ct).startswith("multipart/") 
 
 
-class Request(mapscript.OWSRequest):
-    """ Convenience wrapper class over mapscripts ``OWSRequest``. Provides a 
-        more pythonic interface and adds some convenience functions.
-    """
+class MapServerException(Exception):
+    def __init__(self, message, locator):
+        super(MapServerException, self).__init__(message)
+        self.locator = locator
 
-    def __init__(self, params, type=mapscript.MS_GET_REQUEST):
-        self._used_keys = {}
-
-        self.type = type
-        self.set_parameters(params)
-
-
-    def set_parameters(self, params):
-        if self.type == mapscript.MS_GET_REQUEST:
-            for key, value in params.items():
-                self[key] = value
-        else:
-            self.postrequest = params
-
-
-    def __getitem__(self, key): 
-        if isinstance(key, int):
-            return self.getValue(key)
-        else:
-            return self.getValueByName(key)
-
-
-    def __setitem__(self, key, value):
-        key = key.lower()
-        self._used_keys.setdefault(key, 0)
-
-        # addParameter() available in MapServer >= 6.2 
-        # https://github.com/mapserver/mapserver/issues/3973
-        try:
-            self.addParameter(key.lower(), escape(value))
-        # Workaround for MapServer 6.0
-        except AttributeError:
-            self._used_keys[key] += 1
-            self.setParameter(
-                "%s_%d" % (key, self._used_keys[key]), escape(value)
-            )
-
-
-    def __len__(self):
-        return self.NumParams
-        
 
 class Response(object):
     """ Data class for mapserver results. 
     """
 
     def __init__(self, content, content_type, status):
-        pass
+        self.content = content
+        self.content_type= content_type
 
     @property
     def multipart(self):
@@ -164,7 +125,7 @@ class MetadataMixIn(object):
             return super(MetadataMixIn, self).setMetaData(key, value)
 
 
-class Map(mapscript.mapObj, MetadataMixIn):
+class Map(MetadataMixIn, mapObj):
 
     def dispatch(self, request):
         """ Wraps the ``OWSDispatch`` method. Perfoms all necessary steps for a 
@@ -172,7 +133,7 @@ class Map(mapscript.mapObj, MetadataMixIn):
         """
 
         logger.debug("MapServer: Installing stdout to buffer.")
-        mapscript.msIO_installStdoutToBuffer()
+        msIO_installStdoutToBuffer()
         
         try:
             logger.debug("MapServer: Dispatching.")
@@ -183,20 +144,17 @@ class Map(mapscript.mapObj, MetadataMixIn):
             te = time.time()
             logger.debug("MapServer: Dispatch took %f seconds." % (te - ts))
         except Exception, e:
-            raise InvalidRequestException(
-                str(e),
-                "NoApplicableCode",
-                None
-            )
+            raise MapServerException(str(e), "NoApplicableCode")
         
         logger.debug("MapServer: Retrieving content-type.")
         try:
-            content_type = mapscript.msIO_stripStdoutBufferContentType()
-            mapscript.msIO_stripStdoutBufferContentHeaders()
+            content_type = msIO_stripStdoutBufferContentType()
+            msIO_stripStdoutBufferContentHeaders()
 
-        except mapscript.MapServerError:
+            print content_type
+        except MapServerError:
             # degenerate response. Manually split headers from content
-            result = mapscript.msIO_getStdoutBufferBytes()
+            result = msIO_getStdoutBufferBytes()
             parts = result.split("\r\n")
             result = parts[-1]
             headers = parts[:-1]
@@ -210,26 +168,88 @@ class Map(mapscript.mapObj, MetadataMixIn):
 
         else:
             logger.debug("MapServer: Retrieving stdout buffer bytes.")
-            result = mapscript.msIO_getStdoutBufferBytes()
+            result = msIO_getStdoutBufferBytes()
         
+
+        print result
+
         logger.debug("MapServer: Performing MapServer cleanup.")
         # Workaround for MapServer issue #4369
-        msversion = mapscript.msGetVersionInt()
+        msversion = msGetVersionInt()
         if msversion < 60004 or (msversion < 60200 and msversion >= 60100):
-            mapscript.msCleanup()
+            msCleanup()
         else:
-            mapscript.msIO_resetHandlers()
+            msIO_resetHandlers()
         
-        return Response(result, content_type, dispatch_status)
+        return Response(result, content_type, status)
     
 
 
-class Layer(mapscript.layerObj, MetadataMixIn):
-    def __init__(self, name, metadata=None, type=mapscript.MS_LAYER_RASTER, mapobj=None):
-        mapscript.layerObj.__init__(self, mapobj)
+class Layer(MetadataMixIn, layerObj):
+    def __init__(self, name, metadata=None, type=MS_LAYER_RASTER, mapobj=None):
+        layerObj.__init__(self, mapobj)
         MetadataMixIn.__init__(self, metadata)
-        self.status = mapscript.MS_ON
+        self.status = MS_ON
         if type == MS_LAYER_RASTER:
             self.dump = MS_TRUE
-            self.setConnectionType(mapscript.MS_RASTER, "")
+            self.setConnectionType(MS_RASTER, "")
         self.type = type
+
+
+def create_request(values, request_type=MS_GET_REQUEST):
+    used_keys = {}
+    
+
+    request = OWSRequest()
+    request.type = request_type
+
+    if request_type == MS_GET_REQUEST:
+        for key, value in values:
+            key = key.lower()
+            used_keys.setdefault(key, 0)
+            # addParameter() available in MapServer >= 6.2 
+            # https://github.com/mapserver/mapserver/issues/3973
+            try:
+                request.addParameter(key.lower(), escape(value))
+            # Workaround for MapServer 6.0
+            except AttributeError:
+                used_keys[key] += 1
+                request.setParameter(
+                    "%s_%d" % (key, used_keys[key]), escape(value)
+                )
+    elif request_type == MS_POST_REQUEST:
+        request.postrequest = value
+
+    return request
+
+
+def gdalconst_to_imagemode(const):
+    """
+    This function translates a GDAL data type constant as defined in the
+    :mod:`gdalconst` module to a MapScript image mode constant.
+    """
+    if const == gdal.GDT_Byte:
+        return MS_IMAGEMODE_BYTE
+    elif const in (gdal.GDT_Int16, gdal.GDT_UInt16):
+        return MS_IMAGEMODE_INT16
+    elif const == GDT_Float32:
+        return MS_IMAGEMODE_FLOAT32
+    else:
+        raise InternalError(
+            "MapServer is not capable to process the datatype '%s' (%d)." 
+            % gdal.GetDataTypeName(const), const
+        )
+
+
+def gdalconst_to_imagemode_string(const):
+    """
+    This function translates a GDAL data type constant as defined in the
+    :mod:`gdalconst` module to a string as used in the MapServer map file
+    to denote an image mode.
+    """
+    if const == gdal.GDT_Byte:
+        return "BYTE"
+    elif const in (gdal.GDT_Int16, gdal.GDT_UInt16):
+        return "INT16"
+    elif const == gdal.GDT_Float32:
+        return "FLOAT32"
