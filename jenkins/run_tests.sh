d521bb778a2046561e2298cfd20d253fcc975b6a
Stephan Meissl <stephan@meissl.name>
2013-11-14 16:09:54 +0100

    Updating submodule and moving Referenceable tests to CentOS where the gdal-eox package is available.

--- jenkins/run_tests.sh
+++ jenkins/run_tests.sh
@@ -16,10 +16,11 @@ cd autotest
 export XML_CATALOG_FILES="$WORKSPACE/schemas/catalog.xml"
 # ftp tests are disabled
 if [ $OS == "Ubuntu" ]; then
-    python manage.py test services "coverages|RegisterRemoteDatasetTestCase,RectifiedStitchedMosaicCreateWithRemotePathTestCase" -v2
+    python manage.py test "services|WCS20GetCoverageJPEG2000TestCase,WCS10DescribeCoverageDatasetTestCase,WCS10DescribeCoverageMosaicTestCase,WCS11TransactionReferenceableDatasetTestCase,WCS20GetCoverageReferenceableDatasetGeogCRSSubsetExceedsExtentTestCase,WCS20GetCoverageReferenceableDatasetGeogCRSSubsetTestCase,WCS20GetCoverageReferenceableDatasetImageCRSSubsetTestCase,"
+    python manage.py test "coverages|RegisterRemoteDatasetTestCase,RectifiedStitchedMosaicCreateWithRemotePathTestCase" -v2
 else
-    python manage.py test "services|WCS20GetCoverageOutputCRSotherUoMDatasetTestCase,WCS20GetCoverageReprojectedEPSG3857DatasetTestCase,WCS20GetCoverageJPEG2000TestCase" "coverages|RegisterRemoteDatasetTestCase,RectifiedStitchedMosaicCreateWithRemotePathTestCase" -v2
-    # TODO: Make WCS20GetCoverageJPEG2000TestCase work.
+    python manage.py test "services|WCS20GetCoverageOutputCRSotherUoMDatasetTestCase,WCS20GetCoverageReprojectedEPSG3857DatasetTestCase"
+    python manage.py test "coverages|RegisterRemoteDatasetTestCase,RectifiedStitchedMosaicCreateWithRemotePathTestCase" -v2
 fi
 #TODO: Enable testing of all apps
 #python manage.py test core services coverages backends processes -v2
