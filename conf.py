766e76bf5d7f000a9550536da3cf764418a18e33
Stephan Meissl <stephan@meissl.name>
2013-11-22 12:15:49 +0100

    Adding django settings.

--- conf.py
+++ conf.py
@@ -12,12 +12,17 @@
 # serve to show the default.
 
 import sys, os
+
+from django.conf import settings
+
 from eoxserver import get_version
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.append(os.path.abspath('..'))
+
+settings.configure(DEBUG=True, )
 
 # -- General configuration -----------------------------------------------------
 
