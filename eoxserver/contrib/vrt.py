fc5f384d9a1c83eb19e0cd76a761e10897895185
Fabian Schindler <fabian.schindler@gmx.at>
2013-11-28 11:55:36 +0100

    Implemented vrt dataset builder.

--- eoxserver/contrib/vrt.py
+++ eoxserver/contrib/vrt.py
@@ -26,37 +26,102 @@
 # THE SOFTWARE.
 #-------------------------------------------------------------------------------
 
-from lxml import etree
-from lxml.builder import E
+
+from eoxserver.contrib import gdal
 
 
-class VRT(object):
-    def __init__(self, projection, geotransform, metadata=None):
-        self.projection = projection
-        self.geotransform = geotransform
-        self.metadata = metadata
-        self.bands = []
-        self.gcps = []
-        self.size = (None, None)
+def get_vrt_driver():
+    """ Convenience function to get the VRT driver.
+    """
+    return gdal.GetDriverByName("VRT")
 
 
-    def add_source(self, band, ds):
-        pass
+class VRTBuilder(object):
+    """ A driver for 
+    """
+    def __init__(self, size_x, size_y, num_bands=0, data_type=None, 
+                 vrt_filename=None):
+        driver = get_vrt_driver()
+        data_type = data_type if data_type is not None else gdal.GDT_Byte
+        self._ds = driver.Create(
+            vrt_filename or "", size_x, size_y, num_bands, data_type
+        )
+
+    @classmethod
+    def from_dataset(cls, ds, vrt_filename=None):
+        vrt_builder = cls(
+            ds.RasterXSize, ds.RasterYSize, ds.RasterCount, 
+            vrt_filename=vrt_filename
+        )
+
+        for key, value in ds.GetMetadata().items():
+            vrt_builder.dataset.SetMetadataItem(key, value)
 
 
-    def write(self, file_obj, pretty=False):
-        file_obj.write()
+    @property
+    def dataset(self):
+        return self._ds
 
 
-class VRTRasterBand(list):
-    pass
+    def copy_metadata(self, ds):
+        for key, value in ds.GetMetadata().items():
+            self._ds.SetMetadataItem(key, value)
+
+    def copy_gcps(self, ds, rect=None):
+        gcps = ds.GetGCPs()
+        if rect:
+            gcps = [
+                gdal.GCP(
+                    gcp.GCPX, gcp.GCPY, gcp.GCPZ, gcp.GCPPixel-rect[0], 
+                    gcp.GCPLine-rect[1], gcp.Info, gcp.Id
+                ) for gcp in gcps
+            ]
+        self._ds.SetGCPs(gcps, ds.GetGCPProjection())
 
 
-class SimpleSource(object):
-    def __init__(self, filename, ):
-        pass
+    def add_band(self, data_type=None, options=None):
+        self._ds.AddBand(data_type, options or [])
 
 
-class ComplexSource(object):
-    pass
+    def _add_source_to_band(self, band_index, source):
+        band = self._ds.GetRasterBand(band_index)
+        if not band:
+            raise IndexError
 
+        band.SetMetadataItem("source_0", source, "new_vrt_sources")
+
+
+    def add_simple_source(self, band_index, src, src_band, 
+                          src_rect=None, dst_rect=None):
+        if isinstance(src, str):
+            pass
+
+        else:
+            try:
+                src = src.GetFileList()[0]
+            except AttributeError:
+                raise ValueErrror("Expected string or GDAL Dataset.")
+            except IndexError:
+                raise ValueErrror("Supplied Dataset does not have a filename.")
+
+        lines = [
+            "<SimpleSource>",
+            '<SourceFilename relativeToVRT="1">%s</SourceFilename>' % src,
+            "<SourceBand>%d</SourceBand>" % src_band
+        ]
+        if src_rect:
+            lines.append(
+                '<SrcRect xOff="%d" yOff="%d" xSize="%d" ySize="%d"/>' 
+                % src_rect
+            )
+        if dst_rect:
+            lines.append(
+                '<DstRect xOff="%d" yOff="%d" xSize="%d" ySize="%d"/>' 
+                % dst_rect
+            )
+        lines.append("</SimpleSource>")
+
+        self._add_source_to_band(band_index, "".join(lines))
+
+
+        
