847c37b0b51ddbe685aa75692acd2644021075ce
Stephan Meissl <stephan@meissl.name>
2013-12-20 11:27:20 +0100

    Removing template.map and using eoxserver.conf instead in jenkins tests.

--- jenkins/prepare_instance.sh
+++ jenkins/prepare_instance.sh
@@ -44,17 +44,13 @@ mv autotest tmp1
 eoxserver-admin.py create_instance autotest --init_spatialite
 cp -R tmp1/autotest/data/ autotest/autotest/
 cp -R tmp1/autotest/expected/ autotest/autotest/
-cp tmp1/autotest/conf/template.map autotest/autotest/conf/template.map
+cp tmp1/autotest/conf/eoxserver.conf autotest/autotest/conf/eoxserver.conf
 mkdir -p autotest/autotest/responses/
 cp -R tmp1/autotest_services/ autotest/
 rm -r tmp1
 
-sed -e 's/pdp_type=none/pdp_type=dummypdp/' -i autotest/autotest/conf/eoxserver.conf
-sed -e 's/allowed_actions=/allowed_actions=Add,Delete/' -i autotest/autotest/conf/eoxserver.conf
-sed -e 's/# cache_dir=\/tmp/cache_dir=\/tmp/' -i autotest/autotest/conf/eoxserver.conf
-
-if [ $OS != "Ubuntu" ]; then
-  sed -e 's/#binary_raster_comparison_enabled=false/binary_raster_comparison_enabled=false/' -i autotest/autotest/conf/eoxserver.conf
+if [ $OS == "Ubuntu" ]; then
+  sed -e 's/binary_raster_comparison_enabled=false/binary_raster_comparison_enabled=true/' -i autotest/autotest/conf/eoxserver.conf
 fi
 
 # Enable 'autotest_services' app
