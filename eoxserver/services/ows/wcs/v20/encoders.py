5d3fdf5abd717bf17d4bfa667acf01a794ebdb5a
Fabian Schindler <fabian.schindler@gmx.at>
2014-01-10 11:24:48 +0100

    Fixing lineage tags. Fixing schema locations.Improving footprints when subsetting. Improving alterations of previously create XML (using previous attributes).

--- eoxserver/services/ows/wcs/v20/encoders.py
+++ eoxserver/services/ows/wcs/v20/encoders.py
@@ -30,6 +30,7 @@
 from lxml import etree
 
 from django.contrib.gis.geos import Polygon
+from django.utils.timezone import now
 
 from eoxserver.core.config import get_eoxserver_config
 from eoxserver.core.util.xmltools import XMLEncoder
@@ -548,7 +549,7 @@ class WCS20CoverageDescriptionXMLEncoder(GMLCOV10Encoder):
         ])
 
     def get_schema_locations(self):
-        return nsmap.schema_locations
+        return {ns_wcs.uri: ns_wcs.schema_location}
 
 
 class WCS20EOXMLEncoder(WCS20CoverageDescriptionXMLEncoder, EOP20Encoder, OWS20Encoder):
@@ -562,10 +563,16 @@ class WCS20EOXMLEncoder(WCS20CoverageDescriptionXMLEncoder, EOP20Encoder, OWS20E
                 earth_observation = etree.parse(f).getroot()
 
             if subset_polygon:
-                feature = earth_observation.xpath("eop:featureOfInterest")[0]
-                feature[0] = self.encode_footprint(
-                    subset_polygon, coverage.identifier
-                )
+                try:
+                    feature = earth_observation.xpath(
+                        "eop:featureOfInterest", namespaces=nsmap
+                    )[0]
+                    feature[0] = self.encode_footprint(
+                        coverage.footprint.intersection(subset_polygon),
+                        coverage.identifier
+                    )
+                except IndexError:
+                    pass # no featureOfInterest
 
         else:
             earth_observation = self.encode_earth_observation(
@@ -581,24 +588,24 @@ class WCS20EOXMLEncoder(WCS20CoverageDescriptionXMLEncoder, EOP20Encoder, OWS20E
                     self.encode_reference("Reference",
                         request.build_absolute_uri().replace("&", "&amp;")
                     )
-                )
+                ), GML("timePosition", isoformat(now()))
             )
         elif request.method == "POST": # TODO: better way to do this
             lineage = EOWCS("lineage",
                 EOWCS("referenceGetCoverage",
                     OWS("ServiceReference",
                         OWS("RequestMessage",
-                            etree.parse(request)
-                        )
+                            etree.parse(request).getroot()
+                        ), **{ns_xlink("href"): request.build_absolute_uri().replace("&", "&amp;")}
                     )
-                )
+                ), GML("timePosition", isoformat(now()))
             )
         
         return GMLCOV("metadata",
             GMLCOV("Extension",
                 EOWCS("EOMetadata",
                     earth_observation,
-                    *[lineage] if lineage else []
+                    *[lineage] if lineage is not None else []
                 )
             )
         )
@@ -665,14 +672,14 @@ class WCS20EOXMLEncoder(WCS20CoverageDescriptionXMLEncoder, EOP20Encoder, OWS20E
         return EOWCS("RectifiedDataset", *(
             tree.getchildren() +
             [self.encode_eo_metadata(coverage, request, subset_polygon)]
-        ))
+        ), **tree.attrib)
 
     def alter_rectified_stitched_mosaic(self, coverage, request, subset=None):
         return EOWCS("RectifiedStitchedMosaic", *(
             tree.getchildren() +
             [self.encode_eo_metadata(coverage, request, subset_polygon)]
             # TODO: contributing datasets
-        ))
+        ), **tree.attrib)
 
     def encode_referenceable_dataset(self, coverage, range_type, reference, 
                                      mime_type, subset=None):
@@ -688,7 +695,8 @@ class WCS20EOXMLEncoder(WCS20CoverageDescriptionXMLEncoder, EOP20Encoder, OWS20E
 
         else:
             # subset is given 
-            srid, size, extent, footprint = subset 
+            srid, size, extent, footprint = subset
+            srid = srid if srid is not None else 4326
 
             domain_set = self.encode_domain_set(
                 coverage, srid, size, extent, False
@@ -700,7 +708,8 @@ class WCS20EOXMLEncoder(WCS20CoverageDescriptionXMLEncoder, EOP20Encoder, OWS20E
             # get the WGS84 extent
             poly = Polygon.from_bbox(extent)
             poly.srid = srid
-            poly.transform(dst_srid)
+            if srid != dst_srid:
+                poly.transform(dst_srid)
             extent = poly.extent
             sr = SpatialReference(srid)
 
@@ -753,7 +762,4 @@ class WCS20EOXMLEncoder(WCS20CoverageDescriptionXMLEncoder, EOP20Encoder, OWS20E
         return root
 
     def get_schema_locations(self):
-        return nsmap.schema_locations
-
-
-
+        return {ns_eowcs.uri: ns_eowcs.schema_location}
