bffb119def3183b814bb1846279ba942cb9fbb7c
Martin Paces <martin.paces@eox.at>
2014-02-24 17:34:12 +0100

    Minor refactoring the colorized mask WMS layer factory.

--- eoxserver/services/mapserver/wms/layerfactories/colorized_mask_layer_factory.py
+++ eoxserver/services/mapserver/wms/layerfactories/colorized_mask_layer_factory.py
@@ -55,6 +55,7 @@ class ColorizedMaskLayerFactory(BaseStyleMixIn, AbstractLayerFactory):
     def generate(self, eo_object, group_layer, suffix, options):
         mask_name = suffix[1:]
         coverage = eo_object.cast()
+        layer_name = "%s%s" % (coverage.identifier, suffix)
         
         if mask_name not in self.enabled_masks:
             return
@@ -63,36 +64,46 @@ class ColorizedMaskLayerFactory(BaseStyleMixIn, AbstractLayerFactory):
             semantic="polygonmask[%s]" % mask_name
         )
 
-        if not group_layer and len(mask_items) > 1:
-            group_layer = self.generate_group(coverage.identifier + suffix)
-            yield group_layer, ()
-
-
-        for i, mask_item in enumerate(mask_items):
-            # TODO: datelinewrap?
-            if group_layer:
-                name = "%s%s_%d" % (coverage.identifier, suffix, i)
-            else:
-                name = "%s%s" % (coverage.identifier, suffix)
-
-            layer = self._create_layer(coverage,
-                name, None, #coverage.extent
-            )
+        #layer creating closure 
+        def _polygon_layer( name ) : 
+            layer = self._create_layer(coverage,name,coverage.extent)
             self._set_projection(layer, coverage.spatial_reference)
-            
             layer.type = ms.MS_LAYER_POLYGON
             layer.dump = True
+            layer.offsite = ms.colorObj(0, 0, 0)
+            return layer
 
-            if group_layer:
-                c = group_layer.connection
-                if not c:
-                    group_layer.connection = layer.name
-                else:
-                    group_layer.connection = "%s,%s" % (c, layer.name)
-            else:
-                self.apply_styles(layer, fill=True)
+        # externaly enforced group or multiple groupped masks 
+        if group_layer or len(mask_items) > 1 : 
 
-            yield layer, (mask_item,)
+            # check if the external group provided - if not create a new one 
+            if not group_layer : 
+                group_layer = self.generate_group(coverage.identifier + suffix)
+                yield group_layer, ()
+        
+            # append to a comma separated list 
+            _append = lambda l,v: "%s,%s"%(l,v) if l else v 
+
+            # handle the groupped layers 
+            for i, mask_item in enumerate(mask_items):
+    
+                layer = _polygon_layer("%s_%d"%(layer_name,i))
+
+                group_layer.connection = \
+                    _append( group_layer.connection, layer.name )
+            
+                yield layer, (mask_item,)
+
+
+        # only one mask used directly as a layer 
+        elif len(mask_items) == 1 : 
+
+            layer = _polygon_layer(layer_name)
+
+            yield layer, (mask_item[0],)
+
+        # no mask at all - do nothing 
+        #else : pass 
 
 
     def generate_group(self, name):
