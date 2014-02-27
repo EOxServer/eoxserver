58c9d35a45f7710ad49b25d8a3c26e3600d401ec
pacesm <martin.paces@eox.at>
2014-02-27 12:06:33 +0100

    Fixing the OWS exception hadler for (exception.code==None)
    The exception handler failed for exception's code attribute set to None.
    Further, if not specified the OWS exception code shall default to "NoApplicableCode".

--- eoxserver/services/ows/common/v20/exceptionhandler.py
+++ eoxserver/services/ows/common/v20/exceptionhandler.py
@@ -37,7 +37,9 @@ class OWS20ExceptionHandler(object):
     def handle_exception(self, request, exception):
         message = str(exception)
         version = "2.0.0"
-        code = getattr(exception, "code", type(exception).__name__)
+        # NOTE: An existing 'code' attribute can be set to None value!!!
+        code = getattr(exception, "code", None )
+        if code is None : code = "NoApplicableCode"
         locator = getattr(exception, "locator", None)
         status_code = 400
 
