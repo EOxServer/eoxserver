8bb22c057c86f97d7393998860b49e510ee70118
Martin Paces <martin.paces@eox.at>
2014-04-01 18:17:48 +0200

    WPS: Creating directory to hold default WPS processes. + Adding the 'GetTimeData' process.

---
+++ eoxserver/services/ows/wps/processes/get_time_data.py
@@ -0,0 +1,106 @@
+#-------------------------------------------------------------------------------
+#
+# Project: EOxServer <http://eoxserver.org>
+# Authors: Martin Paces <martin.paces@eox.at>
+#
+#-------------------------------------------------------------------------------
+# Copyright (C) 2014 EOX IT Services GmbH
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies of this Software or works derived from this Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#-------------------------------------------------------------------------------
+
+import csv
+from datetime import datetime
+from StringIO import StringIO
+
+from eoxserver.core import Component, implements
+from eoxserver.core.util.timetools import isoformat
+
+from eoxserver.services.ows.wps.interfaces import ProcessInterface
+from eoxserver.services.ows.wps.parameters import LiteralData, ComplexData
+from eoxserver.services.ows.wps.exceptions import InvalidInputException
+
+from eoxserver.resources.coverages import models
+
+class GetTimeDataProcess(Component):
+    """ GetTimeDataProcess defines a WPS process needed by the EOxClient 
+        time-slider componenet """
+
+    implements(ProcessInterface)
+
+    identifier = "getTimeData"
+    title = "Retrieves time information about a collection"
+    description = "Creates csv output of coverage time information of collections."
+    metadata = [] 
+    profiles = ['EOxServer:GetTimeData']
+
+    inputs = {
+        "collection": str,
+        "begin_time": LiteralData("begin_time",datetime,optional=True),
+        "end_time": LiteralData("end_time",datetime,optional=True),
+    }
+
+    outputs = {
+        "times": str
+    }
+
+    def execute(self, collection, begin_time, end_time):
+        """ The main execution function for the process.
+        """
+
+        # get the dataset series matching the requested ID
+        try: 
+            series = models.DatasetSeries.objects.get( identifier = collection )
+        except models.DatasetSeries.DoesNotExist :
+            raise InvalidInputException( "Invalid collection name '%s' !"%collection ) 
+
+        # recursive dataset series lookup 
+        def _get_children_ids( ds ): 
+            ds_rct= ds.real_content_type 
+            id_list = [ ds.id ] 
+            for child in series.eo_objects.filter(real_content_type=ds_rct) :
+                id_list.extend( _get_children_ids( child ) ) 
+            return id_list 
+
+        series_ids = _get_children_ids( series ) 
+
+        # prepare coverage query set 
+        coverages_qs = models.Coverage.objects.filter(collections__id__in=series_ids)
+        if end_time is not None : 
+            coverages_qs = coverages_qs.filter(begin_time__lte = end_time ) 
+        if begin_time is not None : 
+            coverages_qs = coverages_qs.filter(end_time__gte = begin_time ) 
+        coverages_qs = coverages_qs.order_by('begin_time','end_time') 
+
+        # create the output 
+        output = StringIO()
+        writer = csv.writer(output, quoting=csv.QUOTE_ALL)
+        header = ["starttime", "endtime", "bbox", "identifier" ]
+        writer.writerow(header)
+
+        for coverage in coverages_qs:
+            starttime = coverage.begin_time
+            endtime = coverage.end_time
+            identifier = coverage.identifier
+            bbox = coverage.extent_wgs84
+            writer.writerow([isoformat(starttime), isoformat(endtime), bbox, identifier])
+
+        return output.getvalue()
+        
+
