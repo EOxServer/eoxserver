19f290a4f518502b21f8d01290f6d75516eae5eb
Fabian Schindler <fabian.schindler@gmx.at>
2014-06-26 14:14:25 +0200

    Reworked envisat metadata generation tool.

--- tools/gen_envisat_md.py
+++ tools/gen_envisat_md.py
@@ -3,6 +3,7 @@
 #
 # Project: EOxServer <http://eoxserver.org>
 # Authors: Stephan Krause <stephan.krause@eox.at>
+#          Fabian Schindler <fabian.schindler@eox.at>
 #
 #-------------------------------------------------------------------------------
 # Copyright (C) 2012 EOX IT Services GmbH
@@ -31,129 +32,53 @@
 from osgeo import gdal
 import re
 import os.path
-from sys import argv
+from sys import argv, exit
 from datetime import datetime
+from lxml import etree
+from collections import namedtuple
+
 from django.contrib.gis.geos import GEOSGeometry
-from eoxserver.core.system import System
-from eoxserver.core.util.xmltools import XMLDecoder, DOMElementToXML
-from eoxserver.core.util.timetools import getDateTime, isotime
+
+from eoxserver.core import env
 from eoxserver.processing.gdal.reftools import get_footprint_wkt
-from eoxserver.services.ows.wcs.encoders import GMLEncoder
+from eoxserver.resources.coverages.metadata.formats.gdal_dataset_envisat import (
+    GDALDatasetEnvisatMetadataFormatReader
+)
+from eoxserver.services.gml.v32.encoders import EOP20Encoder
 
-class EOPEncoder(GMLEncoder):
-    def _initializeNamespaces(self):
-        ns_dict = super(EOPEncoder, self)._initializeNamespaces()
-        ns_dict.update({
-            "om": "http://www.opengis.net/om/2.0",
-            "eop": "http://www.opengis.net/eop/2.0"
-        })
-        return ns_dict
 
-    def encodeFootprint(self, footprint, eo_id):
-        return self._makeElement(
-            "eop", "Footprint", [
-                ("@gml", "id", "footprint_%s" % eo_id),
-                ("eop", "multiExtentOf", [
-                    (self.encodeMultiPolygon(footprint, eo_id),)
-                ])
-            ]
-        )
-    
-    def encodeMetadataProperty(self, eo_id):
-        sub_elements =  [
-            ("eop", "identifier", eo_id),
-            ("eop", "acquisitionType", "NOMINAL"), # TODO
-            ("eop", "status", "ARCHIVED") # TODO
-        ]
-        
-        return self._makeElement(
-            "eop", "metaDataProperty", [
-                ("eop", "EarthObservationMetaData", sub_elements)
-            ]
-        )
+EOMetadata = namedtuple("EOMetadata", 
+    ("identifier", "begin_time", "end_time", "footprint")
+)
 
-    def encodeEarthObservation(self, eo_id, begin_time, end_time, footprint):
-        begin_time_iso = isotime(begin_time)
-        end_time_iso = isotime(end_time)
-        result_time_iso = isotime(end_time)
+if __name__ == "__main__":
+    try:
+        path = argv[1]
+    except IndexError:
+        print "Requires filename"
+        exit(1)
 
-        return self._makeElement(
-            "eop", "EarthObservation", [
-                ("@gml", "id", "eop_%s" % eo_id),
-                ("om", "phenomenonTime", [
-                    ("gml", "TimePeriod", [
-                        ("@gml", "id", "phen_time_%s" % eo_id),
-                        ("gml", "beginPosition", begin_time_iso),
-                        ("gml", "endPosition", end_time_iso)
-                    ])
-                ]),
-                ("om", "resultTime", [
-                    ("gml", "TimeInstant", [
-                        ("@gml", "id", "res_time_%s" % eo_id),
-                        ("gml", "timePosition", result_time_iso)
-                    ])
-                ]),
-                ("om", "procedure", []),
-                ("om", "observedProperty", []),
-                ("om", "featureOfInterest", [
-                    (self.encodeFootprint(footprint, eo_id),)
-                ]),
-                ("om", "result", []),
-                (self.encodeMetadataProperty(eo_id),)
-            ]
-        )
-        
-def parse_timestamp(timestamp):
-    MONTHS = {
-        "JAN": 1,
-        "FEB": 2,
-        "MAR": 3,
-        "APR": 4,
-        "MAY": 5,
-        "JUN": 6,
-        "JUL": 7,
-        "AUG": 8,
-        "SEP": 9,
-        "OCT": 10,
-        "NOV": 11,
-        "DEC": 12
-    }
-    
-    m = re.match(r"(\d{2})-([A-Z]{3})-(\d{4}) (\d{2}):(\d{2}):(\d{2}).*", timestamp)
-    day = int(m.group(1))
-    month = MONTHS[m.group(2)]
-    year = int(m.group(3))
-    hour = int(m.group(4))
-    minute = int(m.group(5))
-    second = int(m.group(6))
-    
-    return datetime(year, month, day, hour, minute, second)
-
-if __name__=="__main__":
-    path = argv[1]
-
+    reader = GDALDatasetEnvisatMetadataFormatReader(env)
     ds = gdal.Open(path)
+
+    if not ds:
+        print "Cannot open '%s' as GDAL Dataset." % path
+        exit(1)
+    elif not reader.test_ds(ds):
+        print "Dataset '%s' does not contain required ENVISAT metadata." % path
+        exit(1)
     
-    eo_id = os.path.splitext(os.path.basename(path))[0]
-    begin_time = parse_timestamp(ds.GetMetadataItem("MPH_SENSING_START"))
-    end_time = parse_timestamp(ds.GetMetadataItem("MPH_SENSING_STOP"))
-    
+    md = reader.read_ds(ds)
     del ds
 
-    footprint = GEOSGeometry(get_footprint_wkt(path))
+    footprint = GEOSGeometry(get_footprint_wkt(ds))
     footprint.srid = 4326
 
-    encoder = EOPEncoder()  
+    encoder = EOP20Encoder()
     
-    xml = DOMElementToXML(
-        encoder.encodeEarthObservation(
-            eo_id,
-            begin_time,
-            end_time,
-            footprint
-        )
+    xml = encoder.serialize(
+        encoder.encode_earth_observation(EOMetadata(footprint=footprint, **md))
     )
   
-    xml_file = open(os.path.join(os.path.dirname(path), "%s.xml" % eo_id), "w")
-    xml_file.write(xml)
-    xml_file.close()
+    with open(os.path.join(os.path.dirname(path))) as f:
+        f.write(xml)
