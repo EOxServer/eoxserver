d4e11eca29a39e0757c414c8ae7703a8ce703c1c
martin.paces <martin.paces@eox.at>
2014-08-07 19:18:16 +0200

    CLI: Fixing registration of the RectifiedDatasets.

--- eoxserver/resources/coverages/metadata/formats/gdal_dataset.py
+++ eoxserver/resources/coverages/metadata/formats/gdal_dataset.py
@@ -10,8 +10,8 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all
@@ -34,6 +34,7 @@ from eoxserver.resources.coverages.metadata.interfaces import (
     MetadataReaderInterface, GDALDatasetMetadataReaderInterface
 )
 from eoxserver.processing.gdal import reftools as rt
+from eoxserver.contrib import osr
 
 
 def open_gdal(obj):
@@ -63,65 +64,74 @@ class GDALDatasetMetadataReader(Component):
 
     def read(self, obj):
         ds = open_gdal(obj)
-        if ds is not None:
-            driver = ds.GetDriver()
-            size = (ds.RasterXSize, ds.RasterYSize)
+        if ds is None:
+            raise Exception("Could not parse from obj '%s'." % repr(obj))
+
+        driver = ds.GetDriver()
+        size = (ds.RasterXSize, ds.RasterYSize)
+        values = {"size": size}
+
+        # --= rectified datasets =--
+        # NOTE: If the projection is a non-zero string then
+        #       the geocoding is given by the Geo-Trasnformation
+        #       matrix - not matter what are the values.
+        if ds.GetProjection():
+            values["coverage_type"] = "RectifiedDataset"
+            values["projection"] = (ds.GetProjection(), "WKT")
+
+            # get coordinates of all four image corners
             gt = ds.GetGeoTransform()
-            extent = None
+            def gtrans(x, y):
+                return gt[0] + x*gt[1] + y*gt[2], gt[3] + x*gt[4] + y*gt[5]
+            vpix = [(0, 0), (0, size[1]), (size[0], 0), (size[0], size[1])]
+            vx, vy = zip(*(gtrans(x, y) for x, y in vpix))
 
-            if gt != (0.0, 1.0, 0.0, 0.0, 0.0, 1.0):
-                x_extent = (gt[0], gt[0] + size[0] * gt[1])
-                y_extent = (gt[3] + size[1] * gt[5], gt[3])
+            # find the extent
+            values["extent"] = (min(vx), min(vy), max(vx), max(vy))
 
-                extent = (
-                    min(x_extent),
-                    min(y_extent),
-                    max(x_extent),
-                    max(y_extent)
-                )
-            
-            projection = ds.GetProjection()
+        # --= tie-point encoded referenceable datasets =--
+        # NOTE: If the GCP projection is a non-zero string and
+        #       there are GCPs we are dealing with a tie-point geocoded
+        #       referenceable dataset. The extent is given by the image
+        #       footprint. The fooprint must not be wrapped arround
+        #       the date-line!
+        elif ds.GetGCPProjection() and ds.GetGCPCount() > 0:
+            values["coverage_type"] = "ReferenceableDataset"
+            projection = ds.GetGCPProjection()
+            values["projection"] = (projection, "WKT")
 
-            values = {
-                "size": (ds.RasterXSize, ds.RasterYSize),
-            }
+            # parse the spatial reference to get the EPSG code
+            sr = osr.SpatialReference(projection, "WKT")
 
-            if projection:
-                values["projection"] = (projection, "WKT")
-            if extent:
-                values["extent"] = extent
+            # NOTE: GeosGeometry can't handle non-EPSG geometry projections.
+            if sr.GetAuthorityName(None) == "EPSG":
+                srid = int(sr.GetAuthorityCode(None))
 
-            reader = self._find_additional_reader(ds)
-            if reader:
-                additional_values = reader.read_ds(ds)
-                for key, value in additional_values.items():
-                    values.setdefault(key, value)
-
-            if ds.GetGCPCount() > 0:
+                # get the footprint
                 rt_prm = rt.suggest_transformer(ds)
                 fp_wkt = rt.get_footprint_wkt(ds, **rt_prm)
-                footprint = GEOSGeometry(fp_wkt)
+                footprint = GEOSGeometry(fp_wkt, srid)
 
-                if isinstance(footprint, Polygon):
-                    footprint = MultiPolygon(footprint)
-                elif isinstance(footprint, MultiPolygon):
-                    pass
-                else:
-                    raise Exception(
-                        "Got invalid geometry %s"
-                        % type(footprint).__name__
-                    )
                 values["footprint"] = footprint
+                values["extent"] = footprint.extent
 
-            driver_metadata = driver.GetMetadata()
-            frmt = driver_metadata.get("DMD_MIMETYPE")
-            if frmt:
-                values["format"] = frmt
+        # --= dataset with no geocoding =--
+        # TODO: Handling of other types of GDAL geocoding (e.g, RPC).
+        else:
+            pass
 
-            return values
-            
-        raise Exception("Could not parse from obj '%s'." % repr(obj))
+        reader = self._find_additional_reader(ds)
+        if reader:
+            additional_values = reader.read_ds(ds)
+            for key, value in additional_values.items():
+                values.setdefault(key, value)
 
+        driver_metadata = driver.GetMetadata()
+        frmt = driver_metadata.get("DMD_MIMETYPE")
+        if frmt:
+            values["format"] = frmt
+
+        return values
 
     def _find_additional_reader(self, ds):
         for reader in self.additional_readers:
