dcda6ff1394c1ca00fecebae91688ef956b18d51
Stephan Meissl <stephan@meissl.name>
2015-11-18 14:25:50 +0100

    Install latest pip and pyOpenSSL.

--- vagrant/scripts/packages.sh
+++ vagrant/scripts/packages.sh
@@ -15,13 +15,18 @@ yum install -y gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel \
                sqlite-devel libpng-devel libjpeg-devel libcurl-devel cmake \
                geos-devel fcgi-devel gdal-eox-devel python-devel \
                gdal-eox-driver-envisat gdal-eox-driver-netcdf \
-               gdal-eox-driver-openjpeg2
+               gdal-eox-driver-openjpeg2 libffi-devel
 
 # Attention: Make sure to not install EOxServer from rpm packages!
 # See development_installation.sh for installation.
 
 
-# install recent version of django (1.6, since 1.7+ requires Python 2.7)
+# Install recent version of pip
 yum install -y python-pip
+pip install --upgrade pip
 
+# Install pyOpenSSL to prevent urllib3 InsecurePlatformWarning
+pip install pyopenssl ndg-httpsclient pyasn1
+
+# Install recent version of Django (1.6, since 1.7+ requires Python 2.7)
 pip install "django>=1.6,<1.7"
