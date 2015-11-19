526eda277eeadcbf2253fef52eaa8326d697f429
Martin Paces <martin.paces@eox.at>
2015-11-19 14:43:56 +0100

    (#337) Fixing endless recursion in getTimeData WPS process.

--- eoxserver/services/ows/wps/processes/get_time_data.py
+++ eoxserver/services/ows/wps/processes/get_time_data.py
@@ -88,21 +88,20 @@ class GetTimeDataProcess(Component):
             )
 
         if models.iscollection(model):
-            model = model.cast()
-
             # recursive dataset series lookup
-            def _get_children_ids(ds):
-                ds_rct = ds.real_content_type
-                id_list = [ds.id]
-                for child in model.eo_objects.filter(real_content_type=ds_rct):
-                    id_list.extend(_get_children_ids(child))
+            def _get_children_ids(eoobj):
+                qset = (
+                    eoobj.cast().eo_objects
+                    .filter(real_content_type=eoobj.real_content_type)
+                )
+                id_list = [eoobj.id]
+                for child_eoobj in qset:
+                    id_list.extend(_get_children_ids(child_eoobj))
                 return id_list
-
-            collection_ids = _get_children_ids(model)
 
             # prepare coverage query set
             coverages_qs = models.Coverage.objects.filter(
-                collections__id__in=collection_ids
+                collections__id__in=_get_children_ids(model)
             )
             if end_time is not None:
                 coverages_qs = coverages_qs.filter(begin_time__lte=end_time)
