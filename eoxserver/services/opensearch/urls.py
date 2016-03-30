e274720747b8bc4ef9cdf2376484dc166567a238
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-03-30 10:32:24 +0200

    Using namespaced URLs for OpenSearch.

--- eoxserver/services/opensearch/urls.py
+++ eoxserver/services/opensearch/urls.py
@@ -30,7 +30,7 @@ from django.conf.urls import url
 
 from eoxserver.services.opensearch.views import description, search
 
-urlpatterns = [
-    url(r'^$', description, name='opensearch_description'),
-    url(r'^(?P<collection_id>)$', search, name='opensearch_search')
-]
+urlpatterns = ([
+    url(r'^$', description, name='description'),
+    url(r'^(?P<collection_id>)$', search, name='search')
+], 'opensearch', 'opensearch')
