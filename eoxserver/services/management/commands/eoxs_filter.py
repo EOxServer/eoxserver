52361e29bb1bc7668245c6ddb5df195fa07fe1ac
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2017-04-12 11:22:39 +0200

    Improvin eoxs_filter command. Allowing to set exclusive filtering and printing of available fields.

--- eoxserver/services/management/commands/eoxs_filter.py
+++ eoxserver/services/management/commands/eoxs_filter.py
@@ -16,51 +16,75 @@ class Command(CommandOutputMixIn, BaseCommand):
         make_option('--type', '-t', dest='type', default='EOObject',
             help='Optional. Limit datasets to objects of that type.'
         ),
-        make_option('--debug', '-d', dest="debug",
-            action="store_true", default=False,
+        make_option('--exclude', '-e', dest='exclude',
+            action='store_true', default=False,
             help=(
-                "Optional. Print a representation of the parsed AST of the "
-                "ECQL filter."
+                'Optional. Reverse the lookup: instead of including the matched '
+                'datasets in the result, exclude them and include everything '
+                'else.'
+            )
+        ),
+        make_option('--show-attributes', '--show', '-s', dest='show_attributes',
+            action='store_true', default=False,
+            help=(
+                'Optional. Display the available attributes for the given '
+                'record type.'
             )
         )
     )
 
     args = '<cql-filter>'
 
+    help = """
+        Perform a query of datasets matching the given filters expressed in CQL.
+        The dataset IDs will be written to stdout.
+    """
+
     def handle(self, *args, **options):
         self.verbosity = int(options.get('verbosity', 1))
 
-        if len(args) < 1:
-            raise CommandError('No CQL filter passed.')
-
-        collection_id = options.get('collection_id')
-
+        # get the model class and the field mapping (with choices)
         ModelClass = getattr(models, options.get('type'))
-
         mapping, mapping_choices = get_field_mapping_for_model(ModelClass)
 
-        ast = parse(args[0], mapping, mapping_choices)
+        # print the available attributes, if requested
+        if options.get('show_attributes'):
+            print("\n".join(mapping.keys()))
+            return
 
-        if options.get('debug'):
-            print(get_repr(ast))
-
-        filters = to_filter(ast, mapping, mapping_choices)
-        if not filters:
-            raise CommandError('Invalid filter specified')
-
-        qs = ModelClass.objects.filter(filters)
-        print filters
-
+        # filter by collection, if requested
+        collection_id = options.get('collection_id')
         if collection_id:
             try:
                 collection = models.Collection.objects.get(
                     identifier=collection_id
                 )
-                # qs.
+                qs = ModelClass.objects.filter(collections__in=[collection.pk])
             except models.Collection.DoesNotExist:
                 raise CommandError('No such collection %r' % collection_id)
+        else:
+            qs = ModelClass.objects.all()
+
+        if len(args) < 1:
+            raise CommandError('No CQL filter passed.')
+
+        for arg in args:
+            ast = parse(arg)
+
+            if self.verbosity >= 2:
+                self.print_msg(get_repr(ast), 2)
+
+            filters = to_filter(ast, mapping, mapping_choices)
+            if not filters:
+                raise CommandError('Invalid filter specified')
+
+            if options['exclude']:
+                qs = ModelClass.objects.exclude(filters)
+            else:
+                qs = ModelClass.objects.filter(filters)
+
+            if self.verbosity >= 2:
+                self.print_msg(filters, 2)
 
         qs = qs.values_list('identifier', flat=True)
-        for identifier in qs:
-            print identifier
-
+        print "\n".join(qs)
