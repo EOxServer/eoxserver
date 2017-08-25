ad706325a73fd37e70dfdedb410564c4b84d503f
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2017-08-25 15:54:04 +0200

    Added script to install pysqlite. Adding additional dependencies.

--- vagrant/scripts/packages.sh
+++ vagrant/scripts/packages.sh
@@ -7,7 +7,7 @@ yum update -y
 yum install -y gdal-eox gdal-eox-python postgis proj-epsg python-werkzeug \
                python-lxml mod_wsgi httpd postgresql-server \
                pytz python-dateutil libxml2 libxml2-python mapserver \
-               mapserver-python python-pysqlite-eox
+               mapserver-python python-pysqlite-eox unzip libspatialite
 
 # Install some build dependencies
 yum install -y gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel \
@@ -30,4 +30,4 @@ pip install pyopenssl ndg-httpsclient pyasn1
 
 # Install recent version of Django (1.6, since 1.7+ requires Python 2.7)
 pip install "django>=1.11,<1.12a0" --no-binary django --force-reinstall --upgrade
-pip install django-extensions psycopg2
+pip install django-extensions psycopg2 django-model-utils s2reader
