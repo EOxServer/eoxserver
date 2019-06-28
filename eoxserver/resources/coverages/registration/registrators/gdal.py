adf510f588b5f9255d52cd71447a5153ed452da9
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2019-06-28 15:10:28 +0200

    Fixing axis names when creating a grid

--- eoxserver/resources/coverages/registration/registrators/gdal.py
+++ eoxserver/resources/coverages/registration/registrators/gdal.py
@@ -36,7 +36,7 @@ from eoxserver.resources.coverages.registration.base import BaseRegistrator
 class GDALRegistrator(BaseRegistrator):
     scheme = "GDAL"
 
-    def _read_metadata_from_data(self, data_item, retrieved_metadata, cache):
+    def _read_metadata_from_data(self, data_item, retrieved_metadata, cache, highest_resolution):
         ds = gdal.Open(get_vsi_path(data_item))
         reader = get_reader_by_test(ds)
         if reader:
@@ -47,5 +47,33 @@ class GDALRegistrator(BaseRegistrator):
                 data_item.format = format_
 
             for key, value in values.items():
+                if key == 'origin' and key in retrieved_metadata:
+                    if value != retrieved_metadata[key]:
+                        raise Exception('Different origin')
+
+                if key == 'grid' and key in retrieved_metadata:
+                    old = retrieved_metadata[key]
+                    if isinstance(old, str):
+                        continue
+
+                    if old['axis_names'] != value['axis_names']:
+                        raise Exception('Axis names do not match')
+                    if old['axis_types'] != value['axis_types']:
+                        raise Exception('Axis types do not match')
+                    if old['coordinate_reference_system'] != value['coordinate_reference_system']:
+                        raise Exception('Coordinate reference system does not match')
+
+                    if old['axis_offsets'] != value['axis_offsets']:
+                        if highest_resolution:
+                            if abs(old['axis_offsets'][0]) < abs(value['axis_offsets'][0]):
+                                retrieved_metadata[key] = value
+                        else:
+                            raise Exception("Axis offsets do not match")
+
+                if key == 'size' and key in retrieved_metadata:
+                    old = retrieved_metadata[key]
+                    if old > value:
+                        retrieved_metadata[key] = value
+
                 retrieved_metadata.setdefault(key, value)
         ds = None
