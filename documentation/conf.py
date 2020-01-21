2eb26d93ba90eccceed6193f9f4bef9829d70db0
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2020-01-21 11:53:45 +0100

    Updated docs building and readme

--- documentation/conf.py
+++ documentation/conf.py
@@ -17,12 +17,13 @@ from django.conf import settings
 
 settings.configure(DEBUG=True, )
 
-from eoxserver import get_version
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.append(os.path.abspath('..'))
+
+
+from eoxserver import get_version
 
 # -- General configuration -----------------------------------------------------
 
