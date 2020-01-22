82d78d54aa3d392067cc829eedbab98f522d0808
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2020-01-22 10:04:21 +0100

    Adjustments to autotest settings py as per new instance template

--- autotest/autotest/settings.py
+++ autotest/autotest/settings.py
@@ -43,16 +43,8 @@ from eoxserver.services.ows.wps.config import DEFAULT_EOXS_PROCESSES
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'tmp'
 
-# import django
-# django.setup()
-
 PROJECT_DIR = dirname(abspath(__file__))
 PROJECT_URL_PREFIX = ''
-
-#TODO
-#TEST_RUNNER = 'eoxserver.testing.core.EOxServerTestRunner'
-#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-#TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
 DEBUG = True
 
@@ -75,10 +67,11 @@ if db_type == 'postgis':
         }
     }
 elif db_type == 'spatialite':
+    spatialite_path = os.environ.get('SPATIALITE_PATH', 'data/config.sqlite')
     DATABASES = {
         'default': {
             'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-            'NAME': join(PROJECT_DIR, 'data/config.sqlite'),
+            'NAME': join(PROJECT_DIR, spatialite_path),
         }
     }
 
