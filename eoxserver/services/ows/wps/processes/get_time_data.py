d1e231c1544027758120cf5ff86b055a8417c111
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2020-04-08 13:57:40 +0200

    Adapting 'getTimeData' process: also shows products in a collection

--- eoxserver/services/ows/wps/processes/get_time_data.py
+++ eoxserver/services/ows/wps/processes/get_time_data.py
@@ -1,9 +1,9 @@
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 #
 # Project: EOxServer <http://eoxserver.org>
 # Authors: Martin Paces <martin.paces@eox.at>
 #
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Copyright (C) 2014 EOX IT Services GmbH
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -13,8 +13,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies of this Software or works derived from this Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies of this Software or works derived from this Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -23,11 +23,12 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 import csv
 from datetime import datetime
 
+from django.db.models import Q
 from django.contrib.gis.db.models.functions import Envelope
 
 from eoxserver.core import Component, implements
@@ -51,29 +52,43 @@ class GetTimeDataProcess(Component):
     identifier = "getTimeData"
     title = "Get times of collection coverages."
     decription = ("Query collection and get list of coverages and their times "
-                  "and spatial extents. The process is used by the time-slider "
-                  "of the EOxClient (web client).")
+                  "and spatial extents. The process is used by the "
+                  "time-slider of the EOxClient (web client).")
 
     metadata = {}
     profiles = ['EOxServer:GetTimeData']
 
     inputs = {
-        "collection": LiteralData("collection",
+        "collection": LiteralData(
+            "collection",
             title="Collection name (a.k.a. dataset-series identifier)."),
-        "begin_time": LiteralData("begin_time", datetime, optional=True,
-            title="Optional start of the time interval."),
-        "end_time": LiteralData("end_time", datetime, optional=True,
-            title="Optional end of the time interval."),
+        "begin_time": LiteralData(
+            "begin_time",
+            datetime,
+            optional=True,
+            title="Optional start of the time interval."
+        ),
+        "end_time": LiteralData(
+            "end_time",
+            datetime,
+            optional=True,
+            title="Optional end of the time interval."
+        ),
     }
 
     outputs = {
-        "times": ComplexData("times",
-                    formats=(FormatText('text/csv'), FormatText('text/plain')),
-                    title="Comma separated list of collection's coverages, "
-                          "their extents and times.",
-                    abstract="NOTE: The use of the 'text/plain' format is "
-                             "deprecated! This format will be removed!'"
-                )
+        "times": ComplexData(
+            "times",
+            formats=(FormatText('text/csv'), FormatText('text/plain')),
+            title=(
+                "Comma separated list of collection's coverages, "
+                "their extents and times."
+            ),
+            abstract=(
+                "NOTE: The use of the 'text/plain' format is "
+                "deprecated! This format will be removed!'"
+            )
+        )
     }
 
     @staticmethod
@@ -92,20 +107,39 @@ class GetTimeDataProcess(Component):
             )
 
         if isinstance(model, (models.Collection, models.Mosaic)):
-            # if isinstance(model, models.Collection):
-            coverages_qs = model.coverages
+            if isinstance(model, models.Collection):
+                qs = models.EOObject.objects.filter(
+                    Q(  # coverages
+                        coverage__isnull=False,
+                        coverage__collections=model,
+                    ) | Q (  # products
+                        product__isnull=False,
+                        product__collections=model,
+                    )
+                )
+            else:
+                qs = model.coverages
 
             # prepare coverage query set
             if end_time is not None:
-                coverages_qs = coverages_qs.filter(begin_time__lte=end_time)
+                qs = qs.filter(begin_time__lte=end_time)
             if begin_time is not None:
-                coverages_qs = coverages_qs.filter(end_time__gte=begin_time)
-            coverages_qs = coverages_qs.order_by('begin_time', 'end_time')
-            coverages_qs = coverages_qs.annotate(envelope=Envelope('footprint'))
-            coverages_qs = coverages_qs.values_list("begin_time", "end_time", "identifier", "envelope")
+                qs = qs.filter(end_time__gte=begin_time)
+            qs = qs.order_by(
+                'begin_time', 'end_time'
+            ).annotate(
+                envelope=Envelope('footprint')
+            ).values_list(
+                "begin_time", "end_time", "identifier", "envelope"
+            )
 
         else:
-            coverages_qs = ((model.begin_time, model.end_time, model.identifier, model.footprint),)
+            qs = [(
+                model.begin_time,
+                model.end_time,
+                model.identifier,
+                model.footprint
+            )]
 
         # create the output
         output = CDAsciiTextBuffer()
@@ -113,7 +147,12 @@ class GetTimeDataProcess(Component):
         header = ["starttime", "endtime", "bbox", "identifier"]
         writer.writerow(header)
 
-        for starttime, endtime, identifier, bbox in coverages_qs:
-            writer.writerow([isoformat(starttime), isoformat(endtime), bbox.extent, identifier])
+        for starttime, endtime, identifier, bbox in qs:
+            writer.writerow([
+                isoformat(starttime),
+                isoformat(endtime),
+                bbox.extent,
+                identifier
+            ])
 
         return output
