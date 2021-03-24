bb887e097c39699feb5cad290206e17cbcca86a8
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-03-24 16:56:39 +0100

    By default set the gunicorn workdir to the $INSTANCE_DIR

--- docker/gunicorn.conf.py
+++ docker/gunicorn.conf.py
@@ -1,7 +1,8 @@
+import os
 import multiprocessing
 
 
-chdir = ''
+chdir = os.environ.get('INSTANCE_DIR', '')
 bind = ':8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'eventlet'
