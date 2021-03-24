94c49c7b613838b3cd1ec97326bbd1ef00c99d82
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2021-03-24 16:52:18 +0100

    Allowing to specify 'ALLOWED_HOSTS' via env var

--- eoxserver/instance_template/project_name/settings.py
+++ eoxserver/instance_template/project_name/settings.py
@@ -79,7 +79,10 @@ elif db_type == 'spatialite':
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+if 'ALLOWED_HOSTS' in os.environ:
+    ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split()
+else:
+    ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
