c58a215395b0877be1aca1c0e472f6a18741bd27
baloola <baloola-mu@hotmail.com>
2022-10-20 23:57:44 +0200

    fix the second legend graphics test

--- autotest/autotest_services/tests/wms/test_v13.py
+++ autotest/autotest_services/tests/wms/test_v13.py
@@ -403,7 +403,7 @@ class WMS13GetFeatureInfoEOOMTestCase(testbase.XMLTestCase):
 #===============================================================================
 # Legend Graphic
 #===============================================================================
-class WMS13GetLegendGraphicTestCase(testbase.WMS13GetLegendTestCase):
+class WMS13GetLegendGraphicTestCase(wmsbase.WMS13GetLegendTestCase):
     """ Test a GetLegendGraphic request for a dataset with an associated style. """
 
     def getRequest(self):
