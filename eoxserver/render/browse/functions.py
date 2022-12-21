940ac247e58866592f6eb022e29187bc16fb3a68
Lubomir Dolezal <lubomir.dolezal@eox.at>
2022-12-21 11:02:56 +0100

    feat: update clamp behavior allow to mask out arbitrary nodata range during interpolate

--- eoxserver/render/browse/functions.py
+++ eoxserver/render/browse/functions.py
@@ -28,6 +28,7 @@
 import logging
 from uuid import uuid4
 from functools import wraps
+from typing import List, SupportsFloat as Numeric
 
 import numpy as np
 
@@ -253,26 +254,44 @@ def statistics_stddev(ds, default=0):
     return default
 
 
-def interpolate(ds, x1, x2, y1, y2, clamp=False):
-    """Perform linear interpolation for x between (x1,y1) and (x2,y2) """
+def interpolate(
+        ds:gdal.Dataset, x1:Numeric, x2:Numeric, y1:Numeric, y2:Numeric, clip:bool=False, nodata_range:List[Numeric]=None
+    ):
+    """Perform linear interpolation for x between (x1,y1) and (x2,y2) with
+    optional clamp and additional masking out multiple no data value ranges
+
+    Args:
+        ds (gdal.Dataset): input gdal dataset
+        x1 (Numeric): linear interpolate from min
+        x2 (Numeric): linear interpolate from max
+        y1 (Numeric): linear interpolate to min
+        y2 (Numeric): linear interpolate to max
+        clip (bool, optional): if set to True, performs clip (values below y1 set to y1, values above y2 set to y2). Defaults to False.
+        additional_no_data (List, optional): additionally masks out (sets to band no_data_value) a range of values. Defaults to []. Example [1,5]
+
+    Returns:
+        gdal.Dataset: Interpolated dataset
+    """
     band = ds.GetRasterBand(1)
-    no_data_value = band.GetNoDataValue()
+    nodata_value = band.GetNoDataValue()
     orig_image = band.ReadAsArray()
     # NOTE: the interpolate formula uses large numbers which lead to overflows on uint16
     if orig_image.dtype != convert_dtype(orig_image.dtype):
         orig_image = orig_image.astype(convert_dtype(orig_image.dtype))
     interpolated_image = ((y2 - y1) * orig_image + x2 * y1 - x1 * y2) / (x2 - x1)
-    if clamp:
+    if clip:
         # clamp values below min to min and above max to max
-        interpolated_image[interpolated_image < y1] = y1
-        interpolated_image[interpolated_image > y2] = y2
+        np.clip(interpolated_image, y1, y2, out=interpolated_image)
 
-    if no_data_value is not None:
+    if nodata_value is not None:
         # restore nodata pixels on interpolated array from original array
-        interpolated_image[orig_image == no_data_value] = no_data_value
+        interpolated_image[orig_image == nodata_value] = nodata_value
+        if nodata_range:
+            # apply mask of additional nodata ranges from original array on interpolated array
+            interpolated_image[(orig_image >= nodata_range[0]) & (orig_image <= nodata_range[1])] = nodata_value
 
     ds = gdal_array.OpenNumPyArray(interpolated_image, True)
-    ds.GetRasterBand(1).SetNoDataValue(no_data_value)
+    ds.GetRasterBand(1).SetNoDataValue(nodata_value)
     return ds
 
 
