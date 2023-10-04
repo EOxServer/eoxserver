049d7e405bda0861a9171e1a82f9187a2970427a
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2023-10-04 16:36:11 +0200

    Setting SCL test product to visible

--- autotest/autotest/data/SCL/setup_data.sh
+++ autotest/autotest/data/SCL/setup_data.sh
@@ -10,3 +10,5 @@ python3 manage.py rasterstyle link SCL SCL SCL color
 
 python3 manage.py product register -i S2B_30UUG_20221226_0_L2A -t SCL --footprint "POLYGON ((-6.1994886 55.9041676, -6.1207799 54.9190265, -4.4083987 54.9509423, -4.4439779 55.9372733, -6.1994886 55.9041676))" --replace
 python3 manage.py coverage register -t SCL -r -d autotest/data/SCL/scl_small.tif --footprint-from-extent -i S2B_30UUG_20221226_0_L2A_scl -p S2B_30UUG_20221226_0_L2A
+
+python3 manage.py visibility S2B_30UUG_20221226_0_L2A --wms
